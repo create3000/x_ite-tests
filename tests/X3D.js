59cda875357639b80a302b910a0940e609560ac1
Holger Seelig <holger.seelig@gmail.com>
2023-03-05 14:57:20 +0100

    Added test.

--- tests/X3D.js
+++ tests/X3D.js
@@ -7,4 +7,6 @@ require ("@webcomponents/custom-elements")
 
 global .customElements = window .customElements
 
+const X3D = require ("../../x_ite/dist/x_ite")
+
 module .exports = require ("../../x_ite/dist/x_ite")
---
+++ tests/X_ITE.test.js
@@ -0,0 +1,6 @@
+const X3D = require ("./X3D")
+
+test ("constructor", () =>
+{
+   //console .log (X3D .createBrowser () .browser)
+})
