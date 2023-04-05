f156e89a520bd90d42ca1f4f8e98d2b3d033edd2
Holger Seelig <holger.seelig@gmail.com>
2023-04-05 14:20:35 +0200

    Tests as submodule.

--- tests/X3D.js
+++ tests/X3D.js
@@ -11,6 +11,6 @@ require ("@webcomponents/custom-elements")
 
 global .customElements = window .customElements
 
-const X3D = require ("../../x_ite/dist/x_ite")
+const X3D = require ("../../dist/x_ite")
 
 module .exports = X3D
