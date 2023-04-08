0ada62b6654bbb397f992f69345db44ccbabfeae
Holger Seelig <holger.seelig@gmail.com>
2023-04-08 21:12:37 +0200

    Added tests.

---
+++ tests/x_ite/Fields/SFDouble.test.js
@@ -0,0 +1,47 @@
+const
+   X3D     = require ("../../X3D"),
+   SFDouble = X3D .require ("x_ite/Fields/SFDouble")
+
+test ("constructor", () =>
+{
+   expect (new SFDouble ()                .valueOf ()) .toBe (0)
+   expect (new SFDouble (NaN)             .valueOf ()) .toBe (NaN)
+   expect (new SFDouble (Infinity)        .valueOf ()) .toBe (Infinity)
+   expect (new SFDouble (-Infinity)       .valueOf ()) .toBe (-Infinity)
+   expect (new SFDouble (undefined)       .valueOf ()) .toBe (NaN)
+   expect (new SFDouble (null)            .valueOf ()) .toBe (0)
+   expect (new SFDouble ({})              .valueOf ()) .toBe (NaN)
+   expect (new SFDouble ("")              .valueOf ()) .toBe (0)
+   expect (new SFDouble ("123")           .valueOf ()) .toBe (123)
+   expect (new SFDouble ("123.456")       .valueOf ()) .toBe (123.456)
+   expect (new SFDouble (false)           .valueOf ()) .toBe (0)
+   expect (new SFDouble (true)            .valueOf ()) .toBe (1)
+   expect (new SFDouble (123.456)         .valueOf ()) .toBe (123.456)
+   expect (new SFDouble (123_456_789_012) .valueOf ()) .toBe (123_456_789_012)
+   expect (new SFDouble (0xffffffff)      .valueOf ()) .toBe (0xffffffff)
+   expect (new SFDouble (666)             .valueOf ()) .toBe (666)
+   expect (new SFDouble (-666)            .valueOf ()) .toBe (-666)
+})
+
+test ("setValue", () =>
+{
+   const field = new SFDouble ();
+
+   expect ((field .setValue (),                field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (NaN),             field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (Infinity),        field .valueOf ())) .toBe (Infinity)
+   expect ((field .setValue (-Infinity),       field .valueOf ())) .toBe (-Infinity)
+   expect ((field .setValue (undefined),       field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (null),            field .valueOf ())) .toBe (0)
+   expect ((field .setValue ({}),              field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (""),              field .valueOf ())) .toBe (0)
+   expect ((field .setValue ("123"),           field .valueOf ())) .toBe (123)
+   expect ((field .setValue ("123.456"),       field .valueOf ())) .toBe (123.456)
+   expect ((field .setValue (false),           field .valueOf ())) .toBe (0)
+   expect ((field .setValue (true),            field .valueOf ())) .toBe (1)
+   expect ((field .setValue (123.456),         field .valueOf ())) .toBe (123.456)
+   expect ((field .setValue (123_456_789_012), field .valueOf ())) .toBe (123_456_789_012)
+   expect ((field .setValue (0xffffffff),      field .valueOf ())) .toBe (4294967295)
+   expect ((field .setValue (666),             field .valueOf ())) .toBe (666)
+   expect ((field .setValue (-666),            field .valueOf ())) .toBe (-666)
+})
---
+++ tests/x_ite/Fields/SFTime.test.js
@@ -0,0 +1,47 @@
+const
+   X3D     = require ("../../X3D"),
+   SFTime = X3D .require ("x_ite/Fields/SFTime")
+
+test ("constructor", () =>
+{
+   expect (new SFTime ()                .valueOf ()) .toBe (0)
+   expect (new SFTime (NaN)             .valueOf ()) .toBe (NaN)
+   expect (new SFTime (Infinity)        .valueOf ()) .toBe (Infinity)
+   expect (new SFTime (-Infinity)       .valueOf ()) .toBe (-Infinity)
+   expect (new SFTime (undefined)       .valueOf ()) .toBe (NaN)
+   expect (new SFTime (null)            .valueOf ()) .toBe (0)
+   expect (new SFTime ({})              .valueOf ()) .toBe (NaN)
+   expect (new SFTime ("")              .valueOf ()) .toBe (0)
+   expect (new SFTime ("123")           .valueOf ()) .toBe (123)
+   expect (new SFTime ("123.456")       .valueOf ()) .toBe (123.456)
+   expect (new SFTime (false)           .valueOf ()) .toBe (0)
+   expect (new SFTime (true)            .valueOf ()) .toBe (1)
+   expect (new SFTime (123.456)         .valueOf ()) .toBe (123.456)
+   expect (new SFTime (123_456_789_012) .valueOf ()) .toBe (123_456_789_012)
+   expect (new SFTime (0xffffffff)      .valueOf ()) .toBe (0xffffffff)
+   expect (new SFTime (666)             .valueOf ()) .toBe (666)
+   expect (new SFTime (-666)            .valueOf ()) .toBe (-666)
+})
+
+test ("setValue", () =>
+{
+   const field = new SFTime ();
+
+   expect ((field .setValue (),                field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (NaN),             field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (Infinity),        field .valueOf ())) .toBe (Infinity)
+   expect ((field .setValue (-Infinity),       field .valueOf ())) .toBe (-Infinity)
+   expect ((field .setValue (undefined),       field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (null),            field .valueOf ())) .toBe (0)
+   expect ((field .setValue ({}),              field .valueOf ())) .toBe (NaN)
+   expect ((field .setValue (""),              field .valueOf ())) .toBe (0)
+   expect ((field .setValue ("123"),           field .valueOf ())) .toBe (123)
+   expect ((field .setValue ("123.456"),       field .valueOf ())) .toBe (123.456)
+   expect ((field .setValue (false),           field .valueOf ())) .toBe (0)
+   expect ((field .setValue (true),            field .valueOf ())) .toBe (1)
+   expect ((field .setValue (123.456),         field .valueOf ())) .toBe (123.456)
+   expect ((field .setValue (123_456_789_012), field .valueOf ())) .toBe (123_456_789_012)
+   expect ((field .setValue (0xffffffff),      field .valueOf ())) .toBe (4294967295)
+   expect ((field .setValue (666),             field .valueOf ())) .toBe (666)
+   expect ((field .setValue (-666),            field .valueOf ())) .toBe (-666)
+})
