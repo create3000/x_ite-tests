95c2f77236eba6988f7c386955cf12b3d2480ac0
Holger Seelig <holger.seelig@gmail.com>
2023-05-07 20:33:58 +0200

    Use jest-electron-runner.

--- tests/X3D.js
+++ tests/X3D.js
@@ -1,16 +1,3 @@
-require ("jsdom-global") ()
-require ("@webcomponents/custom-elements")
-
-Object .defineProperty (window, "localStorage",
-{
-   value: new (require ("node-localstorage") .LocalStorage) ("/tmp"),
-   writable: false,
-})
-
-global .require          = require
-global .HTMLElement      = window .HTMLElement
-global .MutationObserver = window .MutationObserver
-global .customElements   = window .customElements
-global .localStorage     = window .localStorage
+// global .require = require
 
 module .exports = require ("../../x_ite/dist/x_ite")
