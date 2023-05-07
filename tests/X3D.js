a40ffb42372dd093ad56002eed6e54971d28a0bb
Holger Seelig <holger.seelig@gmail.com>
2023-05-07 19:41:39 +0200

    Updated global context.

--- tests/X3D.js
+++ tests/X3D.js
@@ -1,11 +1,16 @@
 require ("jsdom-global") ()
 require ("@webcomponents/custom-elements")
 
-//window .HTMLCanvasElement .prototype .getContext = () => null
+Object .defineProperty (window, "localStorage",
+{
+   value: new (require ("node-localstorage") .LocalStorage) ("/tmp"),
+   writable: false,
+})
 
 global .require          = require
 global .HTMLElement      = window .HTMLElement
 global .MutationObserver = window .MutationObserver
 global .customElements   = window .customElements
+global .localStorage     = window .localStorage
 
 module .exports = require ("../../x_ite/dist/x_ite")
