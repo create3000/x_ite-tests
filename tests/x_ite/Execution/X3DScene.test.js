be5ba97bc4d9f97e8e874128d1021c47d8062e19
Holger Seelig <holger.seelig@gmail.com>
2023-05-08 11:01:24 +0200

    Added tests.

--- tests/x_ite/Execution/X3DScene.test.js
+++ tests/x_ite/Execution/X3DScene.test.js
@@ -31,6 +31,17 @@ test ("properties2", () =>
       scene   = Browser .createScene (),
       nodes   = new X3D .MFNode (scene .createNode ("WorldInfo"))
 
+   const
+      specificationVersion = scene .specificationVersion,
+      encoding             = scene .encoding,
+      profile              = scene .profile,
+      components           = scene .components,
+      worldURL             = scene .worldURL,
+      rootNodes            = scene .rootNodes,
+      protos               = scene .protos,
+      externprotos         = scene .externprotos,
+      routes               = scene .routes
+
    scene .specificationVersion = undefined
    scene .encoding             = undefined
    scene .profile              = undefined
@@ -41,14 +52,24 @@ test ("properties2", () =>
    scene .externprotos         = undefined
    scene .routes               = undefined
 
-   expect (scene .specificationVersion) .toMatch (/^\d+\.\d+$/)
-   expect (scene .encoding) .toBe ("SCRIPTED")
-   expect (scene .profile) .toBe (null)
+   expect (scene .specificationVersion) .toBe (specificationVersion)
+   expect (scene .encoding) .toBe (encoding)
+   expect (scene .profile) .toBe (profile)
+   expect (scene .components) .toBe (components)
    expect (scene .components .length) .toBe (0)
-   expect (scene .worldURL) .toMatch (/^file:\/\/\/.*$/)
+   expect (scene .components) .toBeInstanceOf (X3D .ComponentInfoArray)
+   expect (scene .worldURL) .toBe (worldURL)
+   expect (scene .rootNodes) .toBe (rootNodes)
    expect (scene .rootNodes .length) .toBe (1)
    expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo")
+   expect (scene .rootNodes) .toBeInstanceOf (X3D .MFNode)
+   expect (scene .protos) .toBe (protos)
    expect (scene .protos .length) .toBe (0)
+   expect (scene .protos) .toBeInstanceOf (X3D .ProtoDeclarationArray)
+   expect (scene .externprotos) .toBe (externprotos)
    expect (scene .externprotos .length) .toBe (0)
+   expect (scene .externprotos) .toBeInstanceOf (X3D .ExternProtoDeclarationArray)
+   expect (scene .routes) .toBe (routes)
    expect (scene .routes .length) .toBe (0)
+   expect (scene .routes) .toBeInstanceOf (X3D .RouteArray)
 })
