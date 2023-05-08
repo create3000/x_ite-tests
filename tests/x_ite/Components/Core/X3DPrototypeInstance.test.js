043b1e250fc1413a8dc0de1cdaee4461acf0a7f0
Holger Seelig <holger.seelig@gmail.com>
2023-05-08 15:50:04 +0200

    Added tests.

---
+++ tests/x_ite/Components/Core/X3DPrototypeInstance.test.js
@@ -0,0 +1,35 @@
+const X3D = require ("../../../X3D")
+
+test ("setProtoNode", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d+vrml,
+PROFILE Full
+
+PROTO Test [
+   inputOutput SFBool test true
+]
+{
+   Transform { }
+}
+
+Test { }`))
+
+   expect (scene .rootNodes) .toHaveLength (1)
+   expect (scene .rootNodes) .toBeInstanceOf (X3D .MFNode)
+   expect (scene .rootNodes [0]) .toBeInstanceOf (X3D .SFNode)
+   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Test")
+   expect (scene .rootNodes [0]) .toBe (scene .rootNodes [0])
+   expect (scene .rootNodes [0] .metadata) .toBe (null)
+   expect (scene .rootNodes [0] .test) .toBe (true)
+   expect (scene .protos) .toHaveLength (1)
+   expect (scene .protos) .toBeInstanceOf (X3D .ProtoDeclarationArray)
+
+   scene .rootNodes [0] .getValue () .update ()
+
+   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Test")
+   expect (scene .rootNodes [0] .metadata) .toBe (null)
+   expect (scene .rootNodes [0] .test) .toBe (true)
+})
