86ac8ae3722790efa6f03e25af601d458bf7b630
Holger Seelig <holger.seelig@gmail.com>
2023-05-08 17:45:03 +0200

    Added tests.

---
+++ tests/x_ite/Prototype/X3DExternProtoDeclaration.test.js
@@ -0,0 +1,78 @@
+
+const X3D = require ("../../X3D")
+
+test ("properties", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d+vrml,
+PROFILE Interactive
+
+EXTERNPROTO Test [
+   initializeOnly SFVec3f size
+]
+"data:model/x3d+vrml,
+
+PROFILE Interactive
+
+PROTO Test [
+   initializeOnly SFVec3f size 2 2 2
+]
+{
+   DEF I PositionInterpolator { }
+
+   DEF T Transform {
+      children Shape {
+         geometry Box {
+            size IS size
+         }
+      }
+   }
+
+   ROUTE I.value_changed TO T.set_translation
+}"
+
+Test { }
+`))
+
+   expect (scene .externprotos) .toHaveLength (1)
+
+   const externproto = scene .externprotos [0]
+
+   expect (externproto) .toBeInstanceOf (X3D .X3DExternProtoDeclaration)
+   expect (externproto .name) .toBe ("Test")
+   expect (externproto .isExternProto) .toBe (true)
+   expect (externproto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
+   expect (externproto .fields) .toHaveLength (2)
+   expect (externproto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (externproto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
+   expect (externproto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
+   expect (externproto .fields [0] .name) .toBe ("metadata")
+   expect (externproto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (externproto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
+   expect (externproto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
+   expect (externproto .fields [1] .name) .toBe ("size")
+
+   externproto .name = undefined
+   externproto .isExternProto = undefined
+   externproto .fields = undefined
+
+   expect (externproto .name) .toBe ("Test")
+   expect (externproto .isExternProto) .toBe (true)
+   expect (externproto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
+   expect (externproto .fields) .toHaveLength (2)
+   expect (externproto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (externproto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
+   expect (externproto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
+   expect (externproto .fields [0] .name) .toBe ("metadata")
+   expect (externproto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (externproto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
+   expect (externproto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
+   expect (externproto .fields [1] .name) .toBe ("size")
+
+   const instance = externproto .newInstance ()
+
+   expect (instance) .toBeInstanceOf (X3D .SFNode)
+   expect (instance .getNodeTypeName ()) .toBe ("Test")
+})
---
+++ tests/x_ite/Prototype/X3DProtoDeclaration.js
@@ -0,0 +1,71 @@
+
+const X3D = require ("../../X3D")
+
+test ("properties", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d+vrml,
+PROFILE Interactive
+
+PROTO Test [
+   initializeOnly SFVec3f size 2 2 2
+]
+{
+   DEF I PositionInterpolator { }
+
+   DEF T Transform {
+      children Shape {
+         geometry Box {
+            size IS size
+         }
+      }
+   }
+
+   ROUTE I.value_changed TO T.set_translation
+}
+
+Test { }
+`))
+
+   expect (scene .protos) .toHaveLength (1)
+
+   const proto = scene .protos [0]
+
+   expect (proto) .toBeInstanceOf (X3D .X3DProtoDeclaration)
+   expect (proto .name) .toBe ("Test")
+   expect (proto .isExternProto) .toBe (false)
+   expect (proto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
+   expect (proto .fields) .toHaveLength (2)
+   expect (proto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (proto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
+   expect (proto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
+   expect (proto .fields [0] .name) .toBe ("metadata")
+   expect (proto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (proto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
+   expect (proto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
+   expect (proto .fields [1] .name) .toBe ("size")
+
+   proto .name = undefined
+   proto .isExternProto = undefined
+   proto .fields = undefined
+
+   expect (proto .name) .toBe ("Test")
+   expect (proto .isExternProto) .toBe (false)
+   expect (proto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
+   expect (proto .fields) .toHaveLength (2)
+   expect (proto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (proto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
+   expect (proto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
+   expect (proto .fields [0] .name) .toBe ("metadata")
+   expect (proto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
+   expect (proto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
+   expect (proto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
+   expect (proto .fields [1] .name) .toBe ("size")
+
+   const instance = proto .newInstance ()
+
+   expect (instance) .toBeInstanceOf (X3D .SFNode)
+   expect (instance .getNodeTypeName ()) .toBe ("Test")
+})
