3ca908a8bd1a9f31ad418b2ef87cad87b5111b12
Holger Seelig <holger.seelig@gmail.com>
2023-05-08 10:37:06 +0200

    Added tests.

--- tests/x_ite/Execution/X3DScene.test.js
+++ tests/x_ite/Execution/X3DScene.test.js
@@ -1,6 +1,6 @@
 const X3D = require ("../../X3D")
 
-test ("constructor", () =>
+test ("properties1", () =>
 {
    const
       canvas  = X3D .createBrowser (),
@@ -13,6 +13,36 @@ test ("constructor", () =>
    expect (scene .components .length) .toBe (0)
    expect (scene .worldURL) .toMatch (/^file:\/\/\/.*$/)
    expect (scene .rootNodes .length) .toBe (0)
+   expect (scene .protos .length) .toBe (0)
+   expect (scene .externprotos .length) .toBe (0)
+   expect (scene .routes .length) .toBe (0)
+})
+
+test ("properties2", () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = Browser .createScene (),
+      nodes   = new X3D .MFNode (scene .createNode ("WorldInfo"))
+
+   scene .specificationVersion = undefined
+   scene .encoding             = undefined
+   scene .profile              = undefined
+   scene .components           = undefined
+   scene .worldURL             = undefined
+   scene .rootNodes            = nodes
+   scene .protos               = undefined
+   scene .externprotos         = undefined
+   scene .routes               = undefined
+
+   expect (scene .specificationVersion) .toMatch (/^\d+\.\d+$/)
+   expect (scene .encoding) .toBe ("SCRIPTED")
+   expect (scene .profile) .toBe (null)
+   expect (scene .components .length) .toBe (0)
+   expect (scene .worldURL) .toMatch (/^file:\/\/\/.*$/)
+   expect (scene .rootNodes .length) .toBe (1)
+   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo")
    expect (scene .protos .length) .toBe (0)
    expect (scene .externprotos .length) .toBe (0)
    expect (scene .routes .length) .toBe (0)
