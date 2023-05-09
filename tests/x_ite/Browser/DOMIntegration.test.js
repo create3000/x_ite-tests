a0bb5ba2e1aeab9666756839a2843e8a9715216b
Holger Seelig <holger.seelig@gmail.com>
2023-05-09 19:47:41 +0200

    Added tests.

--- tests/x_ite/Browser/DOMIntegration.test.js
+++ tests/x_ite/Browser/DOMIntegration.test.js
@@ -41,7 +41,43 @@ test ("oninitialized", () => new Promise ((resolve, reject) =>
    canvas .on ("error", reject)
 }))
 
-test ("simple-scene", () => new Promise ((resolve, reject) =>
+test ("simple-scene-direct", () => new Promise ((resolve, reject) =>
+{
+   const canvas = $(`<x3d-canvas>
+<X3D profile='Interchange' version='4.0'>
+   <head>
+      <component name='Geometry2D' level='2'></component>
+      <component name='Geospatial' level='2'></component>
+      <component name='HAnim' level='3'></component>
+   </head>
+   <Scene>
+      <Arc2D></Arc2D>
+      <GeoTransform></GeoTransform>
+      <HAnimJoint></HAnimJoint>
+   </Scene>
+</X3D>
+   </x3d-canvas>`)
+
+   const Browser = canvas .prop ("browser")
+
+   expect (canvas .prop ("nodeName")) .toBe ("X3D-CANVAS")
+   expect (Browser) .toBeInstanceOf (X3D .X3DBrowser)
+   expect (Browser .currentScene .rootNodes) .toHaveLength (0)
+
+   canvas .on ("load", () =>
+   {
+      expect (Browser .currentScene .rootNodes) .toHaveLength (3)
+      expect (Browser .currentScene .rootNodes [0] .getNodeTypeName ()) .toBe ("Arc2D")
+      expect (Browser .currentScene .rootNodes [1] .getNodeTypeName ()) .toBe ("GeoTransform")
+      expect (Browser .currentScene .rootNodes [2] .getNodeTypeName ()) .toBe ("HAnimJoint")
+
+      resolve ()
+   })
+
+   canvas .on ("error", reject)
+}))
+
+test ("simple-scene-later", () => new Promise ((resolve, reject) =>
 {
    const
       canvas  = $(`<x3d-canvas></x3d-canvas>`),
