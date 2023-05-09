10904d11367c938b6dbde1204f92a42b79f355cd
Holger Seelig <holger.seelig@gmail.com>
2023-05-09 19:08:11 +0200

    Updated tests.

--- tests/x_ite/Browser/DOMIntegration.test.js
+++ tests/x_ite/Browser/DOMIntegration.test.js
@@ -21,41 +21,33 @@ test ("construction-jquery", () =>
    expect (canvas .browser) .toBeInstanceOf (X3D .X3DBrowser)
 })
 
-test ("onload", () =>
+test ("onload", () => new Promise ((resolve, reject) =>
 {
-   return new Promise ((resolve, reject) =>
-   {
-      const canvas = $(`<x3d-canvas></x3d-canvas>`)
+   const canvas = $(`<x3d-canvas></x3d-canvas>`)
 
-      canvas .html (`<X3D profile='Interchange' version='4.0'><Scene></Scene></X3D>`)
+   canvas .html (`<X3D profile='Interchange' version='4.0'><Scene></Scene></X3D>`)
 
-      canvas .on ("load", resolve)
-      canvas .on ("error", reject)
-   })
-})
+   canvas .on ("load", resolve)
+   canvas .on ("error", reject)
+}))
 
-test ("oninitialized", () =>
+test ("oninitialized", () => new Promise ((resolve, reject) =>
 {
-   return new Promise ((resolve, reject) =>
-   {
-      const canvas = $(`<x3d-canvas></x3d-canvas>`)
+   const canvas = $(`<x3d-canvas></x3d-canvas>`)
 
-      canvas .html (`<X3D profile='Interchange' version='4.0'><Scene></Scene></X3D>`)
+   canvas .html (`<X3D profile='Interchange' version='4.0'><Scene></Scene></X3D>`)
 
-      canvas .on ("initialized", resolve)
-      canvas .on ("error", reject)
-   })
-})
+   canvas .on ("initialized", resolve)
+   canvas .on ("error", reject)
+}))
 
-test ("simple-scene", () =>
+test ("simple-scene", () => new Promise ((resolve, reject) =>
 {
-   return new Promise ((resolve, reject) =>
-   {
-      const
-         canvas  = $(`<x3d-canvas></x3d-canvas>`),
-         Browser = canvas .prop ("browser")
+   const
+      canvas  = $(`<x3d-canvas></x3d-canvas>`),
+      Browser = canvas .prop ("browser")
 
-      canvas .html (`
+   canvas .html (`
 <X3D profile='Interchange' version='4.0'>
    <head>
       <component name='Geometry2D' level='2'></component>
@@ -69,20 +61,19 @@ test ("simple-scene", () =>
    </Scene>
 </X3D>`)
 
-      expect (canvas .prop ("nodeName")) .toBe ("X3D-CANVAS")
-      expect (Browser) .toBeInstanceOf (X3D .X3DBrowser)
-      expect (Browser .currentScene .rootNodes) .toHaveLength (0)
+   expect (canvas .prop ("nodeName")) .toBe ("X3D-CANVAS")
+   expect (Browser) .toBeInstanceOf (X3D .X3DBrowser)
+   expect (Browser .currentScene .rootNodes) .toHaveLength (0)
 
-      canvas .on ("load", () =>
-      {
-         expect (Browser .currentScene .rootNodes) .toHaveLength (3)
-         expect (Browser .currentScene .rootNodes [0] .getNodeTypeName ()) .toBe ("Arc2D")
-         expect (Browser .currentScene .rootNodes [1] .getNodeTypeName ()) .toBe ("GeoTransform")
-         expect (Browser .currentScene .rootNodes [2] .getNodeTypeName ()) .toBe ("HAnimJoint")
+   canvas .on ("load", () =>
+   {
+      expect (Browser .currentScene .rootNodes) .toHaveLength (3)
+      expect (Browser .currentScene .rootNodes [0] .getNodeTypeName ()) .toBe ("Arc2D")
+      expect (Browser .currentScene .rootNodes [1] .getNodeTypeName ()) .toBe ("GeoTransform")
+      expect (Browser .currentScene .rootNodes [2] .getNodeTypeName ()) .toBe ("HAnimJoint")
 
-         resolve ()
-      })
-
-      canvas .on ("error", reject)
+      resolve ()
    })
-})
+
+   canvas .on ("error", reject)
+}))
