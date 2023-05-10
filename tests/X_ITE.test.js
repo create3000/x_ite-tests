d716067a7f720a5f66fb29c08a2a3f19e4e8523b
Holger Seelig <holger.seelig@gmail.com>
2023-05-10 21:56:11 +0200

    Added getBrowser test.

--- tests/X_ITE.test.js
+++ tests/X_ITE.test.js
@@ -1,14 +1,26 @@
 const
+   $                = require ("jquery"),
    X3D              = require ("./X3D"),
    X3DCanvasElement = X3D .require ("x_ite/X3DCanvasElement")
 
-test ("constructor", () =>
+test ("createBrowser", () =>
 {
    const canvas = X3D .createBrowser ()
 
    expect (canvas) .toBeInstanceOf (X3DCanvasElement)
    expect (X3D .getBrowser (canvas)) .toBe (canvas .browser)
    expect (canvas .browser) .toBeInstanceOf (X3D .X3DBrowser)
+})
+
+test ("getBrowser", () =>
+{
+   const canvas = $(X3D .createBrowser ())
+
+   $("body") .append (canvas)
+
+   expect (X3D .getBrowser ()) .toBe (canvas [0] .browser)
+   expect (X3D .getBrowser (canvas [0])) .toBe (canvas [0] .browser)
+   expect (X3D .getBrowser (canvas)) .toBe (canvas [0] .browser)
 })
 
 test ("X3D-classic", () => new Promise ((resolve, reject) =>
