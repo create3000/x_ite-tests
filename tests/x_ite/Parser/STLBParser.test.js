8bf3e3071a7bfb1dc86cd440c11c0b5a5e345619
Holger Seelig <holger.seelig@gmail.com>
2023-05-10 11:16:31 +0200

    Added STLB test.

--- tests/x_ite/Parser/STLBParser.test.js
+++ tests/x_ite/Parser/STLBParser.test.js
@@ -12,7 +12,16 @@ test ("cube", async () =>
       scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", `cube.stl`))))
 
    expect (scene .encoding) .toBe ("STL")
-   expect (scene .rootNodes) .toHaveLength(1)
-   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Shape")
-   expect (scene .rootNodes [0] .geometry .getNodeTypeName ()) .toBe ("TriangleSet")
+   expect (scene .rootNodes) .toHaveLength (1)
+
+   const shape = scene .rootNodes [0]
+
+   expect (shape .getNodeTypeName ()) .toBe ("Shape")
+   expect (shape .appearance .getNodeTypeName ()) .toBe ("Appearance")
+   expect (shape .appearance .material .getNodeTypeName ()) .toBe ("Material")
+   expect (shape .geometry .getNodeTypeName ()) .toBe ("TriangleSet")
+   expect (shape .geometry .normal .getNodeTypeName ()) .toBe ("Normal")
+   expect (shape .geometry .normal .vector) .toHaveLength (12)
+   expect (shape .geometry .coord .getNodeTypeName ()) .toBe ("Coordinate")
+   expect (shape .geometry .coord .point) .toHaveLength (36)
 })
