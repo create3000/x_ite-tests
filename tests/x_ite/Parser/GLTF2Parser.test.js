41cbde399c3c69663e2e0bf86fba0bc6a70aa7a2
Holger Seelig <holger.seelig@gmail.com>
2023-05-11 14:40:37 +0200

    Added glTF test.

---
+++ tests/x_ite/Parser/GLTF2Parser.test.js
@@ -0,0 +1,49 @@
+const
+   path = require ("path"),
+   url  = require ("url")
+
+const X3D = require ("../../X3D")
+
+test ("glTF", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "CesiumMan", "glTF", "CesiumMan.gltf"))))
+
+   expect (scene .encoding) .toBe ("GLTF")
+   expect (scene .rootNodes .length) .toBe (2)
+})
+
+test ("glTF-Binary", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "CesiumMan", "glTF-Binary", "CesiumMan.glb"))))
+
+   expect (scene .encoding) .toBe ("GLTF")
+   expect (scene .rootNodes .length) .toBe (2)
+})
+
+test ("glTF-Draco", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "CesiumMan", "glTF-Draco", "CesiumMan.gltf"))))
+
+   expect (scene .encoding) .toBe ("GLTF")
+   expect (scene .rootNodes .length) .toBe (2)
+})
+
+test ("glTF-Embedded", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "CesiumMan", "glTF-Embedded", "CesiumMan.gltf"))))
+
+   expect (scene .encoding) .toBe ("GLTF")
+   expect (scene .rootNodes .length) .toBe (2)
+})
