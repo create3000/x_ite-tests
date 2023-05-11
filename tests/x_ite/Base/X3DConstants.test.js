70e94077e32dcece9e470fac08c3a1dc93042b17
Holger Seelig <holger.seelig@gmail.com>
2023-05-11 13:02:36 +0200

    Test X3DConstants fields.

--- tests/x_ite/Base/X3DConstants.test.js
+++ tests/x_ite/Base/X3DConstants.test.js
@@ -1,4 +1,6 @@
-const X3D = require ("../../X3D")
+const
+   X3D    = require ("../../X3D"),
+   Fields = X3D .require ("x_ite/Fields")
 
 test ("nodes", async () =>
 {
@@ -10,4 +12,10 @@ test ("nodes", async () =>
 
    for (const Type of Browser .getSupportedNodes ())
       expect (Number .isInteger (X3D .X3DConstants [Type .prototype .getTypeName ()])) .toBe (true)
+})
+
+test ("fields", () =>
+{
+   for (const Field of Object .keys (Fields))
+      expect (Number .isInteger (X3D .X3DConstants [Field])) .toBe (true)
 })
