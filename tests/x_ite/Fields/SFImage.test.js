58b76f599582983e0cfa685576c14c3b117466c1
Holger Seelig <holger.seelig@gmail.com>
2023-05-22 21:51:36 +0200

    Added basic SFImage tests.

---
+++ tests/x_ite/Fields/SFImage.test.js
@@ -0,0 +1,93 @@
+const
+   X3D     = require ("../../X3D"),
+   SFImage = X3D .require ("x_ite/Fields/SFImage"),
+   MFInt32 = X3D .require ("x_ite/Fields") .MFInt32
+
+test ("constructor", () =>
+{
+   const v1 = new SFImage ()
+
+   expect (v1 .x) .toBe (0)
+   expect (v1 .y) .toBe (0)
+   expect (v1 .width) .toBe (0)
+   expect (v1 .height) .toBe (0)
+   expect (v1 .comp) .toBe (0)
+   expect (v1 .array) .toHaveLength (0)
+
+   const v2 = new SFImage (1,2,3,new MFInt32 (5,6))
+
+   expect (v2 .x) .toBe (1)
+   expect (v2 .y) .toBe (2)
+   expect (v2 .width) .toBe (1)
+   expect (v2 .height) .toBe (2)
+   expect (v2 .comp) .toBe (3)
+   expect (v2 .array) .toHaveLength (2)
+   expect (v2 .array [0]) .toBe (5)
+   expect (v2 .array [1]) .toBe (6)
+})
+
+test ("getter/setter", () =>
+{
+   const v1 = new SFImage ()
+
+   v1 .width  = 1
+   v1 .height = 2
+   v1 .comp   = 3
+   v1 .array  = new MFInt32 (5,6)
+
+   expect (v1 .x) .toBe (1)
+   expect (v1 .y) .toBe (2)
+   expect (v1 .width) .toBe (1)
+   expect (v1 .height) .toBe (2)
+   expect (v1 .comp) .toBe (3)
+   expect (v1 .array) .toHaveLength (2)
+   expect (v1 .array [0]) .toBe (5)
+   expect (v1 .array [1]) .toBe (6)
+
+   v1 .x     = 2
+   v1 .y     = 3
+   v1 .comp  = 4
+   v1 .array = new MFInt32 (1,2,3,4,5,6)
+
+   expect (v1 .x) .toBe (2)
+   expect (v1 .y) .toBe (3)
+   expect (v1 .width) .toBe (2)
+   expect (v1 .height) .toBe (3)
+   expect (v1 .comp) .toBe (4)
+   expect (v1 .array) .toHaveLength (6)
+   expect (v1 .array [0]) .toBe (1)
+   expect (v1 .array [1]) .toBe (2)
+   expect (v1 .array [2]) .toBe (3)
+   expect (v1 .array [3]) .toBe (4)
+   expect (v1 .array [4]) .toBe (5)
+   expect (v1 .array [5]) .toBe (6)
+})
+
+test ("common", () =>
+{
+   const field = new SFImage ()
+
+   expect (field .getType ()) .toBe (X3D .X3DConstants .SFImage)
+   expect (field .getTypeName ()) .toBe ("SFImage")
+})
+
+test ("equals", () =>
+{
+   const
+      a = new SFImage (1,2,3),
+      b = new SFImage (2,3,4)
+
+   expect (a .equals (a)) .toBe (true)
+   expect (b .equals (b)) .toBe (true)
+   expect (a .equals (b)) .toBe (false)
+})
+
+test ("isDefaultValue", () =>
+{
+   const
+      a = new SFImage (),
+      b = new SFImage (1,2,3)
+
+   expect (a .isDefaultValue ()) .toBe (true)
+   expect (b .isDefaultValue ()) .toBe (false)
+})
