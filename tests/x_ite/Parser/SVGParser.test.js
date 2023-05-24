16a294d993a09eda26e0e5f3f35b74b757669538
Holger Seelig <holger.seelig@gmail.com>
2023-05-24 23:06:06 +0200

    Added SVG parser test.

--- tests/x_ite/Parser/SVGParser.test.js
+++ tests/x_ite/Parser/SVGParser.test.js
@@ -4,6 +4,17 @@ const
 
 const X3D = require ("../../X3D")
 
+test ("Primitives.svg", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", `Primitives.svg`))))
+
+   expect (scene .encoding) .toBe ("SVG")
+   expect (scene .rootNodes) .toHaveLength (4)
+})
+
 test ("design.svg", async () =>
 {
    const
