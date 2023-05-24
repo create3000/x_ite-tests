c028556114524a1fdc67469b3fc3346033c40b57
Holger Seelig <holger.seelig@gmail.com>
2023-05-24 17:06:37 +0200

    Test matrix inverse and transpose.

--- tests/x_ite/Fields/SFMatrix3.test.js
+++ tests/x_ite/Fields/SFMatrix3.test.js
@@ -145,4 +145,108 @@ for (const Type of Object .keys (X3D .require ("x_ite/Fields/SFMatrix3")))
       expect (a .isDefaultValue ()) .toBe (true)
       expect (b .isDefaultValue ()) .toBe (false)
    })
+
+   test ("get/setTransform", () =>
+   {
+      const
+         a = new SFMatrix3 (),
+         b = new SFMatrix3 ()
+
+      const
+         t = new SFVec2 (),
+         r = new SFVec3 (),
+         s = new SFVec2 (),
+         so = new SFVec3 ()
+
+      a .setTransform (new SFVec2 (2,3), null, new SFVec2 (4,5))
+      expect ([...a]) .toEqual ([4,0,0, 0,5,0, 2,3,1])
+
+      a .setTransform (new SFVec2 (2,3), 4, new SFVec2 (4,5), 6)
+      a .getTransform (t,r,s,so)
+      b .setTransform (t,r.z,s,so.z)
+
+      expect (b [0]) .toBeCloseTo (a [0])
+      expect (b [1]) .toBeCloseTo (a [1])
+      expect (b [2]) .toBeCloseTo (a [2])
+      expect (b [3]) .toBeCloseTo (a [3])
+      expect (b [4]) .toBeCloseTo (a [4])
+      expect (b [5]) .toBeCloseTo (a [5])
+      expect (b [6]) .toBeCloseTo (a [6])
+      expect (b [7]) .toBeCloseTo (a [7])
+      expect (b [8]) .toBeCloseTo (a [8])
+
+      a .setTransform ()
+      expect ([...a]) .toEqual ([1,0,0,0,1,0,0,0,1])
+   })
+
+   test ("inverse", () =>
+   {
+      const a = new SFMatrix3 ()
+
+      a .setTransform (new SFVec2 (2,3), 5, new SFVec2 (4,5), 4)
+
+      const
+         b = a .multRight (a .inverse ()),
+         c = a .multLeft (a .inverse ())
+
+      expect (a) .not .toBe (b)
+      expect (a) .not .toBe (c)
+      expect (b) .not .toBe (c)
+      expect (b) .toBeInstanceOf (SFMatrix3)
+      expect (c) .toBeInstanceOf (SFMatrix3)
+
+      expect (b [0]) .toBeCloseTo (1)
+      expect (b [1]) .toBeCloseTo (0)
+      expect (b [2]) .toBeCloseTo (0)
+      expect (b [3]) .toBeCloseTo (0)
+      expect (b [4]) .toBeCloseTo (1)
+      expect (b [5]) .toBeCloseTo (0)
+      expect (b [6]) .toBeCloseTo (0)
+      expect (b [7]) .toBeCloseTo (0)
+      expect (b [8]) .toBeCloseTo (1)
+
+      expect (c [0]) .toBeCloseTo (1)
+      expect (c [1]) .toBeCloseTo (0)
+      expect (c [2]) .toBeCloseTo (0)
+      expect (c [3]) .toBeCloseTo (0)
+      expect (c [4]) .toBeCloseTo (1)
+      expect (c [5]) .toBeCloseTo (0)
+      expect (c [6]) .toBeCloseTo (0)
+      expect (c [7]) .toBeCloseTo (0)
+      expect (c [8]) .toBeCloseTo (1)
+   })
+
+   test ("transpose", () =>
+   {
+      const
+         a = new SFMatrix3 (2,3,4, 5,6,7, 8,9,10),
+         b = a .transpose (),
+         c = b .transpose ()
+
+      expect (a) .not .toBe (b)
+      expect (a) .not .toBe (c)
+      expect (b) .not .toBe (c)
+      expect (b) .toBeInstanceOf (SFMatrix3)
+      expect (c) .toBeInstanceOf (SFMatrix3)
+
+      expect (b [0]) .toBe (2)
+      expect (b [3]) .toBe (3)
+      expect (b [6]) .toBe (4)
+      expect (b [1]) .toBe (5)
+      expect (b [4]) .toBe (6)
+      expect (b [7]) .toBe (7)
+      expect (b [2]) .toBe (8)
+      expect (b [5]) .toBe (9)
+      expect (b [8]) .toBe (10)
+
+      expect (c [0]) .toBe (2)
+      expect (c [1]) .toBe (3)
+      expect (c [2]) .toBe (4)
+      expect (c [3]) .toBe (5)
+      expect (c [4]) .toBe (6)
+      expect (c [5]) .toBe (7)
+      expect (c [6]) .toBe (8)
+      expect (c [7]) .toBe (9)
+      expect (c [8]) .toBe (10)
+   })
 }
--- tests/x_ite/Fields/SFMatrix4.test.js
+++ tests/x_ite/Fields/SFMatrix4.test.js
@@ -12,6 +12,8 @@ const SFVec4Type = {
    VrmlMatrix: "SFVec4f",
 }
 
+const SFRotation = X3D .SFRotation
+
 for (const Type of Object .keys (X3D .require ("x_ite/Fields/SFMatrix4")))
 {
    const
@@ -23,16 +25,16 @@ for (const Type of Object .keys (X3D .require ("x_ite/Fields/SFMatrix4")))
    {
       const v1 = new SFMatrix4 ()
 
-      expect (v1 [0]) .toBe (1)
-      expect (v1 [1]) .toBe (0)
-      expect (v1 [2]) .toBe (0)
-      expect (v1 [3]) .toBe (0)
-      expect (v1 [4]) .toBe (0)
-      expect (v1 [5]) .toBe (1)
-      expect (v1 [6]) .toBe (0)
-      expect (v1 [7]) .toBe (0)
-      expect (v1 [8]) .toBe (0)
-      expect (v1 [9]) .toBe (0)
+      expect (v1 [ 0]) .toBe (1)
+      expect (v1 [ 1]) .toBe (0)
+      expect (v1 [ 2]) .toBe (0)
+      expect (v1 [ 3]) .toBe (0)
+      expect (v1 [ 4]) .toBe (0)
+      expect (v1 [ 5]) .toBe (1)
+      expect (v1 [ 6]) .toBe (0)
+      expect (v1 [ 7]) .toBe (0)
+      expect (v1 [ 8]) .toBe (0)
+      expect (v1 [ 9]) .toBe (0)
       expect (v1 [10]) .toBe (1)
       expect (v1 [11]) .toBe (0)
       expect (v1 [12]) .toBe (0)
@@ -43,16 +45,16 @@ for (const Type of Object .keys (X3D .require ("x_ite/Fields/SFMatrix4")))
 
       const v2 = new SFMatrix4 (2,3,4,5, 6,7,8,9, 10,11,12,13, 14,15,16,17)
 
-      expect (v2 [0]) .toBe (2)
-      expect (v2 [1]) .toBe (3)
-      expect (v2 [2]) .toBe (4)
-      expect (v2 [3]) .toBe (5)
-      expect (v2 [4]) .toBe (6)
-      expect (v2 [5]) .toBe (7)
-      expect (v2 [6]) .toBe (8)
-      expect (v2 [7]) .toBe (9)
-      expect (v2 [8]) .toBe (10)
-      expect (v2 [9]) .toBe (11)
+      expect (v2 [ 0]) .toBe (2)
+      expect (v2 [ 1]) .toBe (3)
+      expect (v2 [ 2]) .toBe (4)
+      expect (v2 [ 3]) .toBe (5)
+      expect (v2 [ 4]) .toBe (6)
+      expect (v2 [ 5]) .toBe (7)
+      expect (v2 [ 6]) .toBe (8)
+      expect (v2 [ 7]) .toBe (9)
+      expect (v2 [ 8]) .toBe (10)
+      expect (v2 [ 9]) .toBe (11)
       expect (v2 [10]) .toBe (12)
       expect (v2 [11]) .toBe (13)
       expect (v2 [12]) .toBe (14)
@@ -181,5 +183,144 @@ for (const Type of Object .keys (X3D .require ("x_ite/Fields/SFMatrix4")))
 
       expect (a .isDefaultValue ()) .toBe (true)
       expect (b .isDefaultValue ()) .toBe (false)
+   })
+
+   test ("get/setTransform", () =>
+   {
+      const
+         a = new SFMatrix4 (),
+         b = new SFMatrix4 ()
+
+      const
+         t = new SFVec3 (),
+         r = new SFRotation (),
+         s = new SFVec3 (),
+         so = new SFRotation ()
+
+      a .setTransform (new SFVec3 (1,2,3), null, new SFVec3 (4,5,6))
+      expect ([...a]) .toEqual ([4,0,0,0, 0,5,0,0, 0,0,6,0, 1,2,3,1])
+
+      a .setTransform (new SFVec3 (1,2,3), new SFRotation (1,2,3,4), new SFVec3 (4,5,6), new SFRotation (2,3,4,5))
+      a .getTransform (t,r,s,so)
+      b .setTransform (t,r,s,so)
+
+      expect (b [ 0]) .toBeCloseTo (a [ 0])
+      expect (b [ 1]) .toBeCloseTo (a [ 1])
+      expect (b [ 2]) .toBeCloseTo (a [ 2])
+      expect (b [ 3]) .toBeCloseTo (a [ 3])
+      expect (b [ 4]) .toBeCloseTo (a [ 4])
+      expect (b [ 5]) .toBeCloseTo (a [ 5])
+      expect (b [ 6]) .toBeCloseTo (a [ 6])
+      expect (b [ 7]) .toBeCloseTo (a [ 7])
+      expect (b [ 8]) .toBeCloseTo (a [ 8])
+      expect (b [ 9]) .toBeCloseTo (a [ 9])
+      expect (b [10]) .toBeCloseTo (a [10])
+      expect (b [11]) .toBeCloseTo (a [11])
+      expect (b [12]) .toBeCloseTo (a [12])
+      expect (b [13]) .toBeCloseTo (a [13])
+      expect (b [14]) .toBeCloseTo (a [14])
+      expect (b [15]) .toBeCloseTo (a [15])
+
+      a .setTransform ()
+      expect ([...a]) .toEqual ([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1])
+   })
+
+   test ("inverse", () =>
+   {
+      const a = new SFMatrix4 ()
+
+      a .setTransform (new SFVec3 (2,3,4), new SFRotation (5,6,7,8), new SFVec3 (4,5,6), new SFRotation (6,7,8,9))
+
+      const
+         b = a .multRight (a .inverse ()),
+         c = a .multLeft (a .inverse ())
+
+      expect (a) .not .toBe (b)
+      expect (a) .not .toBe (c)
+      expect (b) .not .toBe (c)
+      expect (b) .toBeInstanceOf (SFMatrix4)
+      expect (c) .toBeInstanceOf (SFMatrix4)
+
+      expect (b [ 0]) .toBeCloseTo (1)
+      expect (b [ 1]) .toBeCloseTo (0)
+      expect (b [ 2]) .toBeCloseTo (0)
+      expect (b [ 3]) .toBeCloseTo (0)
+      expect (b [ 4]) .toBeCloseTo (0)
+      expect (b [ 5]) .toBeCloseTo (1)
+      expect (b [ 6]) .toBeCloseTo (0)
+      expect (b [ 7]) .toBeCloseTo (0)
+      expect (b [ 8]) .toBeCloseTo (0)
+      expect (b [ 9]) .toBeCloseTo (0)
+      expect (b [10]) .toBeCloseTo (1)
+      expect (b [11]) .toBeCloseTo (0)
+      expect (b [12]) .toBeCloseTo (0)
+      expect (b [13]) .toBeCloseTo (0)
+      expect (b [14]) .toBeCloseTo (0)
+      expect (b [15]) .toBeCloseTo (1)
+
+      expect (c [ 0]) .toBeCloseTo (1)
+      expect (c [ 1]) .toBeCloseTo (0)
+      expect (c [ 2]) .toBeCloseTo (0)
+      expect (c [ 3]) .toBeCloseTo (0)
+      expect (c [ 4]) .toBeCloseTo (0)
+      expect (c [ 5]) .toBeCloseTo (1)
+      expect (c [ 6]) .toBeCloseTo (0)
+      expect (c [ 7]) .toBeCloseTo (0)
+      expect (c [ 8]) .toBeCloseTo (0)
+      expect (c [ 9]) .toBeCloseTo (0)
+      expect (c [10]) .toBeCloseTo (1)
+      expect (c [11]) .toBeCloseTo (0)
+      expect (c [12]) .toBeCloseTo (0)
+      expect (c [13]) .toBeCloseTo (0)
+      expect (c [14]) .toBeCloseTo (0)
+      expect (c [15]) .toBeCloseTo (1)
+   })
+
+   test ("transpose", () =>
+   {
+      const
+         a = new SFMatrix4 (2,3,4,5, 6,7,8,9, 10,11,12,13, 14,15,16,17),
+         b = a .transpose (),
+         c = b .transpose ()
+
+      expect (a) .not .toBe (b)
+      expect (a) .not .toBe (c)
+      expect (b) .not .toBe (c)
+      expect (b) .toBeInstanceOf (SFMatrix4)
+      expect (c) .toBeInstanceOf (SFMatrix4)
+
+      expect (b [ 0]) .toBe ( 2)
+      expect (b [ 4]) .toBe ( 3)
+      expect (b [ 8]) .toBe ( 4)
+      expect (b [12]) .toBe ( 5)
+      expect (b [ 1]) .toBe ( 6)
+      expect (b [ 5]) .toBe ( 7)
+      expect (b [ 9]) .toBe ( 8)
+      expect (b [13]) .toBe ( 9)
+      expect (b [ 2]) .toBe (10)
+      expect (b [ 6]) .toBe (11)
+      expect (b [10]) .toBe (12)
+      expect (b [14]) .toBe (13)
+      expect (b [ 3]) .toBe (14)
+      expect (b [ 7]) .toBe (15)
+      expect (b [11]) .toBe (16)
+      expect (b [15]) .toBe (17)
+
+      expect (c [ 0]) .toBe ( 2)
+      expect (c [ 1]) .toBe ( 3)
+      expect (c [ 2]) .toBe ( 4)
+      expect (c [ 3]) .toBe ( 5)
+      expect (c [ 4]) .toBe ( 6)
+      expect (c [ 5]) .toBe ( 7)
+      expect (c [ 6]) .toBe ( 8)
+      expect (c [ 7]) .toBe ( 9)
+      expect (c [ 8]) .toBe (10)
+      expect (c [ 9]) .toBe (11)
+      expect (c [10]) .toBe (12)
+      expect (c [11]) .toBe (13)
+      expect (c [12]) .toBe (14)
+      expect (c [13]) .toBe (15)
+      expect (c [14]) .toBe (16)
+      expect (c [15]) .toBe (17)
    })
 }
