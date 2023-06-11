251bca88a2276784fcf3b42dd54a63ff3b3d2fb8
Holger Seelig <holger.seelig@gmail.com>
2023-06-11 15:03:46 +0200

    Added first Script node tests.

---
+++ tests/x_ite/Components/Scripting/Script.test.js
@@ -0,0 +1,52 @@
+const
+   X3D    = require ("../../../X3D"),
+   Fields = X3D .require ("x_ite/Fields")
+
+const
+   canvas  = X3D .createBrowser (),
+   Browser = canvas .browser
+
+test ("environment", async () =>
+{
+   await Browser .loadComponents (Browser .getComponent ("Scripting"))
+
+   const
+      Script = Browser .getConcreteNode ("Script"),
+      script = new Script (Browser .currentScene)
+
+   script ._url = new X3D .MFString ("ecmascript:")
+   script .setup ()
+
+   expect (script .evaluate ("Browser")) .toBe (Browser)
+   expect (script .evaluate ("X3DConstants")) .toBe (X3D .X3DConstants)
+})
+
+test ("fields", async () =>
+{
+   await Browser .loadComponents (Browser .getComponent ("Scripting"))
+
+   const
+      Script = Browser .getConcreteNode ("Script"),
+      script = new Script (Browser .currentScene)
+
+   script .addUserDefinedField (X3D .X3DConstants .initializeOnly, "double1", new Fields .SFDouble ())
+   script .addUserDefinedField (X3D .X3DConstants .initializeOnly, "vector1", new Fields .SFVec3f ())
+   script .addUserDefinedField (X3D .X3DConstants .inputOnly, "double2", new Fields .SFDouble ())
+   script .addUserDefinedField (X3D .X3DConstants .inputOnly, "vector2", new Fields .SFVec3f ())
+   script .addUserDefinedField (X3D .X3DConstants .outputOnly, "double3", new Fields .SFDouble ())
+   script .addUserDefinedField (X3D .X3DConstants .outputOnly, "vector3", new Fields .SFVec3f ())
+   script .addUserDefinedField (X3D .X3DConstants .inputOutput, "double4", new Fields .SFDouble ())
+   script .addUserDefinedField (X3D .X3DConstants .inputOutput, "vector4", new Fields .SFVec3f ())
+
+   script ._url = new X3D .MFString ("ecmascript:")
+   script .setup ()
+
+   expect (script .evaluate ("double1")) .toBe (0)
+   expect (script .evaluate ("vector1")) .toBeInstanceOf (Fields .SFVec3f)
+   expect (script .evaluate ("typeof double2")) .toBe ("undefined")
+   expect (script .evaluate ("typeof vector2")) .toBe ("undefined")
+   expect (script .evaluate ("double3")) .toBe (0)
+   expect (script .evaluate ("vector3")) .toBeInstanceOf (Fields .SFVec3f)
+   expect (script .evaluate ("double4")) .toBe (0)
+   expect (script .evaluate ("vector4")) .toBeInstanceOf (Fields .SFVec3f)
+})
