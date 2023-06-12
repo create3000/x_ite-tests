2198cadbe25ba4c99dd33f140c7c772515538bd9
Holger Seelig <holger.seelig@gmail.com>
2023-06-12 21:42:42 +0200

    Don't test for specification version.

--- tests/x_ite/Components/Scripting/Script.test.js
+++ tests/x_ite/Components/Scripting/Script.test.js
@@ -89,7 +89,11 @@ DEF Script Script {
 
       const script = scene .getNamedNode ("Script") .getValue ()
 
-      expect (() => script .evaluate ("new SFNode ('Transform { }')")) .toThrow (Error)
+      expect (script .evaluate ("SFNode")) .not .toBe (X3D .SFNode)
+      expect (script .evaluate ("new SFNode ('Transform { }')")) .toBeInstanceOf (script .evaluate ("SFNode"))
+      expect (script .evaluate ("new SFNode ('Transform { }')")) .toBeInstanceOf (X3D .SFNode)
+      expect (script .evaluate ("new SFNode ('Transform { }')") .getNodeTypeName ()) .toBe ("Transform")
+      expect (() => script .evaluate ("new SFNode ('NULL')")) .toThrow (Error)
       expect (browser .getScriptStack ()) .toHaveLength (1)
    }
    catch (error)
