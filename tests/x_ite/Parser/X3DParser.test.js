23603ecc2d02101aed22092757ffb53cbc8eee2e
Holger Seelig <holger.seelig@gmail.com>
2023-07-03 11:17:22 +0200

    Test Generator styles.

--- tests/x_ite/Parser/X3DParser.test.js
+++ tests/x_ite/Parser/X3DParser.test.js
@@ -14,35 +14,40 @@ test ("statements.x3d", async () =>
       latestVersion = Browser .createScene () .specificationVersion,
       scene         = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "X3D", `statements.x3d`))))
 
-   const
-      orig = await fetch (path .join (__dirname, "files", "X3D", `statements.x3d`)) .then (r => r .text ()),
-      x3d  = scene .toXMLString (),
-      x3dv = scene .toVRMLString (),
-      x3dj = scene .toJSONString ()
+   const orig = await fetch (path .join (__dirname, "files", "X3D", `statements.x3d`)) .then (r => r .text ())
 
-   const encodings = ["XML", "XML", "VRML", "JSON"]
-
-   Browser .baseURL = scene .worldURL
-
-   for (const [i, file] of [orig, x3d, x3dv, x3dj] .entries ())
+   for (const style of ["TIDY", "COMPACT", "SMALL", "CLEAN"])
    {
-      const scene = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d,${file}`))
+      const
+         x3d  = scene .toXMLString  ({ style: style }),
+         x3dv = scene .toVRMLString ({ style: style }),
+         x3dj = scene .toJSONString ({ style: style })
 
-      expect (scene .encoding) .toBe (encodings [i])
+      const encodings = ["XML", "XML", "VRML", "JSON"]
 
-      if (i)
-         expect (scene .specificationVersion) .toBe (latestVersion)
+      Browser .baseURL = scene .worldURL
 
-      expect (scene .getNamedNode ("Transform") .rotation .angle) .toBeCloseTo (Math .PI / 4)
-      expect (scene .getNamedNode ("Box") .size .x) .toBeCloseTo (4)
-      expect (scene .getNamedNode ("Box") .size .y) .toBeCloseTo (4)
-      expect (scene .getNamedNode ("Box") .size .z) .toBeCloseTo (4)
-      expect (scene .getNamedNode ("Emitter") .mass) .toBeCloseTo (2)
-      expect (scene .getNamedNode ("Force") .force .y) .toBeCloseTo (-10)
+      for (const [i, file] of [orig, x3d, x3dv, x3dj] .entries ())
+      {
+         const scene = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d,${file}`))
 
-      expect (scene .toXMLString ()) .toBe (orig)
-      expect (scene .toVRMLString ()) .toBe (x3dv)
-      expect (scene .toJSONString ()) .toBe (x3dj)
+         expect (scene .encoding) .toBe (encodings [i])
+
+         if (i)
+            expect (scene .specificationVersion) .toBe (latestVersion)
+
+         expect (scene .getNamedNode ("Transform") .rotation .angle) .toBeCloseTo (Math .PI / 4)
+         expect (scene .getNamedNode ("Box") .size .x) .toBeCloseTo (4)
+         expect (scene .getNamedNode ("Box") .size .y) .toBeCloseTo (4)
+         expect (scene .getNamedNode ("Box") .size .z) .toBeCloseTo (4)
+         expect (scene .getNamedNode ("Emitter") .mass) .toBeCloseTo (2)
+         expect (scene .getNamedNode ("Force") .force .y) .toBeCloseTo (-10)
+
+         expect (scene .toXMLString ()) .toBe (orig)
+         expect (scene .toXMLString  ({ style: style })) .toBe (x3d)
+         expect (scene .toVRMLString ({ style: style })) .toBe (x3dv)
+         expect (scene .toJSONString ({ style: style })) .toBe (x3dj)
+      }
    }
 })
 
@@ -52,28 +57,33 @@ test ("fields.x3d", async () =>
       latestVersion = Browser .createScene () .specificationVersion,
       scene         = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "X3D", `fields.x3d`))))
 
-   const
-      orig = await fetch (path .join (__dirname, "files", "X3D", `fields.x3d`)) .then (r => r .text ()),
-      x3d  = scene .toXMLString (),
-      x3dv = scene .toVRMLString (),
-      x3dj = scene .toJSONString ()
+   const orig = await fetch (path .join (__dirname, "files", "X3D", `fields.x3d`)) .then (r => r .text ())
 
-   const encodings = ["XML", "XML", "VRML", "JSON"]
-
-   Browser .baseURL = scene .worldURL
-
-   for (const [i, file] of [orig, x3d, x3dv, x3dj] .entries ())
+   for (const style of ["TIDY", "COMPACT", "SMALL", "CLEAN"])
    {
-      const scene = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d,${file}`))
+      const
+         x3d  = scene .toXMLString  ({ style: style }),
+         x3dv = scene .toVRMLString ({ style: style }),
+         x3dj = scene .toJSONString ({ style: style })
 
-      expect (scene .encoding) .toBe (encodings [i])
+      const encodings = ["XML", "XML", "VRML", "JSON"]
 
-      if (i)
-         expect (scene .specificationVersion) .toBe (latestVersion)
+      Browser .baseURL = scene .worldURL
 
-      expect (scene .toXMLString ()) .toBe (x3d)
-      expect (scene .toVRMLString ()) .toBe (x3dv)
-      expect (scene .toJSONString ()) .toBe (x3dj)
+      for (const [i, file] of [orig, x3d, x3dv, x3dj] .entries ())
+      {
+         const scene = await Browser .createX3DFromURL (new X3D .MFString (`data:model/x3d,${file}`))
+
+         expect (scene .encoding) .toBe (encodings [i])
+
+         if (i)
+            expect (scene .specificationVersion) .toBe (latestVersion)
+
+         expect (scene .toXMLString ()) .toBe (orig)
+         expect (scene .toXMLString  ({ style: style })) .toBe (x3d)
+         expect (scene .toVRMLString ({ style: style })) .toBe (x3dv)
+         expect (scene .toJSONString ({ style: style })) .toBe (x3dj)
+      }
    }
 })
 
