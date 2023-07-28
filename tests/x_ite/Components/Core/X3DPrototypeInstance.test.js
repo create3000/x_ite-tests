8234d8544bd0998e6fa2f6e250b26a15117ced24
Holger Seelig <holger.seelig@gmail.com>
2023-07-28 02:07:52 +0200

    Test setProtoNode.

--- tests/x_ite/Components/Core/X3DPrototypeInstance.test.js
+++ tests/x_ite/Components/Core/X3DPrototypeInstance.test.js
@@ -29,6 +29,9 @@ Test { }`))
    expect (scene .protos) .toBeInstanceOf (X3D .ProtoDeclarationArray)
    expect (scene .rootNodes [0] .getValue () .getBody () .rootNodes) .toHaveLength (1)
    expect (scene .rootNodes [0] .getValue () .getBody () .rootNodes [0] .getNodeTypeName ()) .toBe ("Transform")
+   expect (scene .rootNodes [0] .getFieldDefinitions ()) .toHaveLength (2)
+   expect (scene .rootNodes [0] .getFieldDefinitions () [0] .value .getValue ()) .toBe (null)
+   expect (scene .rootNodes [0] .getFieldDefinitions () [1] .value .getValue ()) .toBe (true)
 
    const rootNodes = scene .rootNodes [0] .getValue () .getBody () .rootNodes
 
@@ -41,6 +44,66 @@ Test { }`))
    expect (scene .rootNodes [0] .getValue () .getBody () .rootNodes [0] .getNodeTypeName ()) .toBe ("Transform")
    expect (scene .rootNodes [0] .getValue () .getBody () .rootNodes) .not .toBe (rootNodes)
    expect (scene .rootNodes [0] .getValue () .getBody () .rootNodes [0]) .not .toBe (rootNodes [0])
+   expect (scene .rootNodes [0] .getFieldDefinitions ()) .toHaveLength (2)
+   expect (scene .rootNodes [0] .getFieldDefinitions () [0] .value .getValue ()) .toBe (null)
+   expect (scene .rootNodes [0] .getFieldDefinitions () [1] .value .getValue ()) .toBe (true)
+})
+
+test ("setProtoNode", async () =>
+{
+   const scene = await Browser .createX3DFromString (`
+PROFILE Interchange
+
+PROTO Test1 [
+   inputOutput SFInt32 test1 123
+]
+{
+   Transform { }
+}
+
+PROTO Test2 [
+   inputOutput SFInt32 test2 456
+]
+{
+   Group { }
+}
+
+Test1 { }`)
+
+   const
+      proto1   = scene .protos [0],
+      proto2   = scene .protos [1],
+      instance = scene .rootNodes [0] .getValue ()
+
+   expect (instance .getTypeName ()) .toBe ("Test1")
+   expect (instance .getField ("test1") .getValue ()) .toBe (123)
+   expect (instance .getFieldDefinitions ()) .toHaveLength (2)
+   expect (instance .getFieldDefinitions () [0] .name) .toBe ("metadata")
+   expect (instance .getFieldDefinitions () [1] .name) .toBe ("test1")
+   expect (instance .getFieldDefinitions () [0] .value .getValue ()) .toBe (null)
+   expect (instance .getFieldDefinitions () [1] .value .getValue ()) .toBe (123)
+
+   instance .setProtoNode (proto2)
+
+   expect (instance .getTypeName ()) .toBe ("Test2")
+   expect (instance .getField ("test2") .getValue ()) .toBe (456)
+   expect (() => instance .getField ("test1")) .toThrow (Error)
+   expect (instance .getFieldDefinitions ()) .toHaveLength (2)
+   expect (instance .getFieldDefinitions () [0] .name) .toBe ("metadata")
+   expect (instance .getFieldDefinitions () [1] .name) .toBe ("test2")
+   expect (instance .getFieldDefinitions () [0] .value .getValue ()) .toBe (null)
+   expect (instance .getFieldDefinitions () [1] .value .getValue ()) .toBe (456)
+
+   instance .setProtoNode (proto1)
+
+   expect (instance .getTypeName ()) .toBe ("Test1")
+   expect (instance .getField ("test1") .getValue ()) .toBe (123)
+   expect (() => instance .getField ("test2")) .toThrow (Error)
+   expect (instance .getFieldDefinitions ()) .toHaveLength (2)
+   expect (instance .getFieldDefinitions () [0] .name) .toBe ("metadata")
+   expect (instance .getFieldDefinitions () [1] .name) .toBe ("test1")
+   expect (instance .getFieldDefinitions () [0] .value .getValue ()) .toBe (null)
+   expect (instance .getFieldDefinitions () [1] .value .getValue ()) .toBe (123)
 })
 
 test ("static-properties", () =>
