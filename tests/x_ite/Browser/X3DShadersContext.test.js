fc54f02ec8f3e893134ae16a91ab6d174806eb3b
Holger Seelig <holger.seelig@gmail.com>
2023-08-01 20:03:43 +0200

    Test more shader options.

--- tests/x_ite/Browser/X3DShadersContext.test.js
+++ tests/x_ite/Browser/X3DShadersContext.test.js
@@ -39,49 +39,56 @@ test ("shader", () =>
                   options .push ("X3D_LOGARITHMIC_DEPTH_BUFFER");
                   options .push (`X3D_GEOMETRY_${geometry}`);
                   options .push (fogs [i % fogs .length]);
-                  options .push ("X3D_COLOR_MATERIAL");
-                  options .push ("X3D_NORMALS");
-                  options .push ("X3D_ALPHA_MODE_OPAQUE", "X3D_ALPHA_MODE_MASK");
-                  options .push ("X3D_CLIP_PLANES")
-                  options .push ("X3D_NUM_CLIP_PLANES 1");
-                  options .push ("X3D_PROJECTIVE_TEXTURE_MAPPING")
-                  options .push (`X3D_NUM_TEXTURE_PROJECTORS ${numTextureXXX}`);
-                  options .push ("X3D_STYLE_PROPERTIES");
-                  options .push ("X3D_SKINNING")
 
-                  if (materialTexture)
+                  for (const o of [true, false])
                   {
-                     options .push ("X3D_MATERIAL_TEXTURES");
-                     options .push ("X3D_EMISSIVE_TEXTURE", `X3D_EMISSIVE_TEXTURE_${texture}`);
-                     options .push ("X3D_NORMAL_TEXTURE", `X3D_NORMAL_TEXTURE_${texture}`);
+                     if (o)
+                     {
+                        options .push ("X3D_COLOR_MATERIAL");
+                        options .push ("X3D_NORMALS");
+                        options .push ("X3D_ALPHA_MODE_OPAQUE", "X3D_ALPHA_MODE_MASK");
+                        options .push ("X3D_CLIP_PLANES")
+                        options .push ("X3D_NUM_CLIP_PLANES 1");
+                        options .push ("X3D_PROJECTIVE_TEXTURE_MAPPING")
+                        options .push (`X3D_NUM_TEXTURE_PROJECTORS ${numTextureXXX}`);
+                        options .push ("X3D_STYLE_PROPERTIES");
+                        options .push ("X3D_SKINNING")
+                     }
 
-                     options .push ("X3D_AMBIENT_TEXTURE", `X3D_AMBIENT_TEXTURE_${texture}`);
-                     options .push ("X3D_DIFFUSE_TEXTURE", `X3D_DIFFUSE_TEXTURE_${texture}`);
-                     options .push ("X3D_SPECULAR_TEXTURE", `X3D_SPECULAR_TEXTURE_${texture}`);
-                     options .push ("X3D_SHININESS_TEXTURE", `X3D_SHININESS_TEXTURE_${texture}`);
+                     if (materialTexture)
+                     {
+                        options .push ("X3D_MATERIAL_TEXTURES");
+                        options .push ("X3D_EMISSIVE_TEXTURE", `X3D_EMISSIVE_TEXTURE_${texture}`);
+                        options .push ("X3D_NORMAL_TEXTURE", `X3D_NORMAL_TEXTURE_${texture}`);
 
-                     options .push ("X3D_OCCLUSION_TEXTURE", `X3D_OCCLUSION_TEXTURE_${texture}`);
+                        options .push ("X3D_AMBIENT_TEXTURE", `X3D_AMBIENT_TEXTURE_${texture}`);
+                        options .push ("X3D_DIFFUSE_TEXTURE", `X3D_DIFFUSE_TEXTURE_${texture}`);
+                        options .push ("X3D_SPECULAR_TEXTURE", `X3D_SPECULAR_TEXTURE_${texture}`);
+                        options .push ("X3D_SHININESS_TEXTURE", `X3D_SHININESS_TEXTURE_${texture}`);
 
-                     options .push ("X3D_BASE_TEXTURE", `X3D_BASE_TEXTURE_${texture}`);
-                     options .push ("X3D_METALLIC_ROUGHNESS_TEXTURE", `X3D_METALLIC_ROUGHNESS_TEXTURE_${texture}`);
+                        options .push ("X3D_OCCLUSION_TEXTURE", `X3D_OCCLUSION_TEXTURE_${texture}`);
+
+                        options .push ("X3D_BASE_TEXTURE", `X3D_BASE_TEXTURE_${texture}`);
+                        options .push ("X3D_METALLIC_ROUGHNESS_TEXTURE", `X3D_METALLIC_ROUGHNESS_TEXTURE_${texture}`);
+                     }
+                     else
+                     {
+                        options .push ("X3D_TEXTURE");
+                        options .push ("X3D_MULTI_TEXTURING");
+                        options .push (`X3D_NUM_TEXTURES ${numTextureXXX}`);
+                        options .push (`X3D_TEXTURE0_${texture}`);
+                        options .push (`X3D_TEXTURE1_${texture}`);
+                     }
+
+                     options .push (`X3D_NUM_TEXTURE_TRANSFORMS ${numTextureXXX}`);
+                     options .push (`X3D_NUM_TEXTURE_COORDINATES ${numTextureXXX}`);
+
+                     const shader = Browser .createShader (name, vertex, fragment, options);
+
+                     expect (shader .isValid ()) .toBe (true)
+
+                     ++ i;
                   }
-                  else
-                  {
-                     options .push ("X3D_TEXTURE");
-                     options .push ("X3D_MULTI_TEXTURING");
-                     options .push (`X3D_NUM_TEXTURES ${numTextureXXX}`);
-                     options .push (`X3D_TEXTURE0_${texture}`);
-                     options .push (`X3D_TEXTURE1_${texture}`);
-                  }
-
-                  options .push (`X3D_NUM_TEXTURE_TRANSFORMS ${numTextureXXX}`);
-                  options .push (`X3D_NUM_TEXTURE_COORDINATES ${numTextureXXX}`);
-
-                  const shader = Browser .createShader (name, vertex, fragment, options);
-
-                  expect (shader .isValid ()) .toBe (true)
-
-                  ++ i;
                }
             }
          }
