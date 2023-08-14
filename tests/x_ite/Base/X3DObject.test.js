b8b13096a176070145499fbc96dfca00670ed41a
Holger Seelig <holger.seelig@gmail.com>
2023-08-14 02:14:48 +0200

    Test user data.

--- tests/x_ite/Base/X3DObject.test.js
+++ tests/x_ite/Base/X3DObject.test.js
@@ -12,3 +12,28 @@ test ("getId", () =>
    expect (X3DObject .getId ({ })) .not .toBe ({ })
    expect (Browser .getId ()) .not .toBe (scene .getId ())
 })
+
+test ("get/setUserData", () =>
+{
+   const s = Symbol (), o = { }
+
+   expect (Browser .getUserData (123)) .toBe (undefined)
+   expect (Browser .getUserData (s))   .toBe (undefined)
+   expect (Browser .getUserData (o))   .toBe (undefined)
+
+   Browser .setUserData (123, 456)
+   Browser .setUserData (s, "string")
+   Browser .setUserData (o, "object")
+
+   expect (Browser .getUserData (123)) .toBe (456)
+   expect (Browser .getUserData (s))   .toBe ("string")
+   expect (Browser .getUserData (o))   .toBe ("object")
+
+   Browser .removeUserData (123)
+   Browser .removeUserData (s)
+   Browser .removeUserData (o)
+
+   expect (Browser .getUserData (123)) .toBe (undefined)
+   expect (Browser .getUserData (s))   .toBe (undefined)
+   expect (Browser .getUserData (o))   .toBe (undefined)
+})
