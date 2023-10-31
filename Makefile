2c3ebed2561236901e1f2e6f4621240d09db47f4
Holger Seelig <holger.seelig@gmail.com>
2023-10-31 22:17:37 +0100

    Prepare test.

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 test-docu:
 	perl tests/x_ite/Components/Core/fields.pl
+
+test-x3duom:
+	perl tests/x_ite/Components/Core/x3duom.pl
---
+++ tests/x_ite/Components/Core/x3duom.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use v5.10.0;
+use utf8;
+use open qw/:std :utf8/;
+
+# https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml
+
+say "Tested against X3DUOM";
