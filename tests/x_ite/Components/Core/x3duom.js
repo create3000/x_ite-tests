e906a3b39013fa9015099b28c59d6c8839c00584
Holger Seelig <holger.seelig@gmail.com>
2023-11-01 16:48:52 +0100

    Filter nodes.

--- tests/x_ite/Components/Core/x3duom.js
+++ tests/x_ite/Components/Core/x3duom.js
@@ -1,21 +1,39 @@
 #!/usr/bin/env node
 
+const
+   path = require ("path");
+
 // https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml
 
-const x3doum = xml (sh `wget -q -O - https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml`);
-
-console .log (x3doum);
+const
+   x3doum = xml (sh `wget -q -O - https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml`),
+   nodes  = new Map (x3doum .X3dUnifiedObjectModel .ConcreteNodes .ConcreteNode .map (node => [node .name, node]));
 
 sh `find ${process .cwd ()}/../x_ite/src/x_ite/Components -type f -mindepth 2`
    .split ("\n")
    .sort ()
+   .slice (10, 11)
    .forEach (s => node (s));
 
 console .log ("Test done.");
 
 function node (filename)
 {
-   // console .log (filename);
+   const typeName = path .parse (filename) .name;
+
+   // Filter abstract nodes.
+
+   if (typeName .match (/^X3D/))
+      return;
+
+   const x3doum = nodes .get (typeName);
+
+   // Filter non-standard nodes.
+
+   if (!x3doum)
+      return;
+
+   console .log (typeName, x3doum);
 }
 
 function sh (strings, ... values)
@@ -29,7 +47,10 @@ function xml (string)
 {
    const { XMLParser } = require ("fast-xml-parser")
 
-   const parser = new XMLParser ();
+   const parser = new XMLParser ({
+      ignoreAttributes: false,
+      attributeNamePrefix: "",
+   });
 
    return parser .parse (string);
 }
