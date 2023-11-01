6168347add6ac5407e31e1486b5e2a6c3a29ac40
Holger Seelig <holger.seelig@gmail.com>
2023-11-01 15:43:34 +0100

    Parse XML.

--- tests/x_ite/Components/Core/x3duom.js
+++ tests/x_ite/Components/Core/x3duom.js
@@ -2,7 +2,11 @@
 
 // https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml
 
-sh`find ${process .cwd ()}/../x_ite/src/x_ite/Components -type f -mindepth 2`
+const x3doum = xml (sh `wget -q -O - https://www.web3d.org/specifications/X3dUnifiedObjectModel-4.0.xml`);
+
+console .log (x3doum);
+
+sh `find ${process .cwd ()}/../x_ite/src/x_ite/Components -type f -mindepth 2`
    .split ("\n")
    .sort ()
    .forEach (s => node (s));
@@ -11,12 +15,21 @@ console .log ("Test done.");
 
 function node (filename)
 {
-   console .log (filename);
+   // console .log (filename);
 }
 
 function sh (strings, ... values)
 {
    const { execSync } = require ("child_process");
 
-   return execSync (String .raw ({ raw: strings }, ... values), { encoding: "utf8" });
+   return execSync (String .raw ({ raw: strings }, ... values), { encoding: "utf8", maxBuffer: 100 * 1024 * 1024 });
+}
+
+function xml (string)
+{
+   const { XMLParser } = require ("fast-xml-parser")
+
+   const parser = new XMLParser ();
+
+   return parser .parse (string);
 }
