fa1cea6b8659a090448da50567a7eda3213b7ea5
Holger Seelig <holger.seelig@gmail.com>
2023-11-03 12:16:15 +0100

    Add semicolon.

--- tests/x_ite/Components/Core/X3DNode.test.js
+++ tests/x_ite/Components/Core/X3DNode.test.js
@@ -1,14 +1,14 @@
-const path = require ("path")
+const path = require ("path");
 
 test ("documentation", () =>
 {
-   expect (sh `perl "${path .resolve (__dirname, "documentation.pl")}"`) .toBe ("Test done.\n")
+   expect (sh `perl "${path .resolve (__dirname, "documentation.pl")}"`) .toBe ("Test done.\n");
 })
 
 test ("X3DUOM", () =>
 {
    expect (sh `node "${path .resolve (__dirname, "x3duom.js")}"`)
-      .toBe (sh `cat "${path .resolve (__dirname, "x3duom.txt")}"`)
+      .toBe (sh `cat "${path .resolve (__dirname, "x3duom.txt")}"`);
 })
 
 function sh (strings, ... values)
