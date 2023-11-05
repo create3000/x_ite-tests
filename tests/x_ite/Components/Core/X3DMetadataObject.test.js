6869795c1b0be4e42c0557799b5ef20b85f1ef06
Holger Seelig <holger.seelig@gmail.com>
2023-11-05 11:32:26 +0100

    Test parse/toXMLString metadata.

--- tests/x_ite/Components/Core/X3DMetadataObject.test.js
+++ tests/x_ite/Components/Core/X3DMetadataObject.test.js
@@ -6,7 +6,7 @@ const
 
 test ("containerField-3.3", async () =>
 {
-   const scene = await Browser .createX3DFromString (/* xml */ `<?xml version="1.0" encoding="UTF-8"?>
+   const scene1 = await Browser .createX3DFromString (/* xml */ `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.3//EN" "http://www.web3d.org/specifications/x3d-3.3.dtd">
 <X3D profile='Core' version='3.3'>
    <Scene>
@@ -21,17 +21,26 @@ test ("containerField-3.3", async () =>
 </X3D>
    `);
 
-   expect (scene .rootNodes) .toHaveLength (1);
-   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
-   expect (scene .rootNodes [0] .metadata) .not .toBe (null);
-   expect (scene .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
-   expect (scene .rootNodes [0] .metadata .value) .toHaveLength (1);
-   expect (scene .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+   expect (scene1 .rootNodes) .toHaveLength (1);
+   expect (scene1 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
+   expect (scene1 .rootNodes [0] .metadata) .not .toBe (null);
+   expect (scene1 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene1 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene1 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+
+   const scene2 = await Browser .createX3DFromString (scene1 .toXMLString ());
+
+   expect (scene2 .rootNodes) .toHaveLength (1);
+   expect (scene2 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
+   expect (scene2 .rootNodes [0] .metadata) .not .toBe (null);
+   expect (scene2 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene2 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene2 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
 });
 
 test ("containerField-4.0", async () =>
 {
-   const scene = await Browser .createX3DFromString (/* xml */ `<?xml version="1.0" encoding="UTF-8"?>
+   const scene1 = await Browser .createX3DFromString (/* xml */ `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 4.0//EN" "http://www.web3d.org/specifications/x3d-4.0.dtd">
 <X3D profile='Core' version='4.0'>
    <Scene>
@@ -46,10 +55,19 @@ test ("containerField-4.0", async () =>
 </X3D>
    `);
 
-   expect (scene .rootNodes) .toHaveLength (1);
-   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
-   expect (scene .rootNodes [0] .metadata) .not .toBe (null);
-   expect (scene .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
-   expect (scene .rootNodes [0] .metadata .value) .toHaveLength (1);
-   expect (scene .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+   expect (scene1 .rootNodes) .toHaveLength (1);
+   expect (scene1 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
+   expect (scene1 .rootNodes [0] .metadata) .not .toBe (null);
+   expect (scene1 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene1 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene1 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+
+   const scene2 = await Browser .createX3DFromString (scene1 .toXMLString ());
+
+   expect (scene2 .rootNodes) .toHaveLength (1);
+   expect (scene2 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
+   expect (scene2 .rootNodes [0] .metadata) .not .toBe (null);
+   expect (scene2 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene2 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene2 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
 });
