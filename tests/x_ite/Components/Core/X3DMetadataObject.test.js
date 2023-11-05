42eb43e3f1a803825f3dc22ca15b70dfc1ce9b6e
Holger Seelig <holger.seelig@gmail.com>
2023-11-05 11:56:59 +0100

    Test parse/toXMLString metadata.

--- tests/x_ite/Components/Core/X3DMetadataObject.test.js
+++ tests/x_ite/Components/Core/X3DMetadataObject.test.js
@@ -11,10 +11,13 @@ test ("containerField-3.3", async () =>
 <X3D profile='Core' version='3.3'>
    <Scene>
       <WorldInfo>
-         <MetadataSet
-               name='metadata'>
-            <MetadataBoolean containerField='value'
-                  name='value'/>
+         <MetadataSet>
+            <MetadataBoolean containerField='value'/>
+            <MetadataDouble  containerField='value'/>
+            <MetadataFloat   containerField='value'/>
+            <MetadataInteger containerField='value'/>
+            <MetadataSet     containerField='value'/>
+            <MetadataString  containerField='value'/>
          </MetadataSet>
       </WorldInfo>
    </Scene>
@@ -26,8 +29,13 @@ test ("containerField-3.3", async () =>
    expect (scene1 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
    expect (scene1 .rootNodes [0] .metadata) .not .toBe (null);
    expect (scene1 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
-   expect (scene1 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene1 .rootNodes [0] .metadata .value) .toHaveLength (6);
    expect (scene1 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+   expect (scene1 .rootNodes [0] .metadata .value [1] .getNodeTypeName ()) .toBe ("MetadataDouble");
+   expect (scene1 .rootNodes [0] .metadata .value [2] .getNodeTypeName ()) .toBe ("MetadataFloat");
+   expect (scene1 .rootNodes [0] .metadata .value [3] .getNodeTypeName ()) .toBe ("MetadataInteger");
+   expect (scene1 .rootNodes [0] .metadata .value [4] .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene1 .rootNodes [0] .metadata .value [5] .getNodeTypeName ()) .toBe ("MetadataString");
 
    const scene2 = await Browser .createX3DFromString (scene1 .toXMLString ());
 
@@ -36,8 +44,13 @@ test ("containerField-3.3", async () =>
    expect (scene2 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
    expect (scene2 .rootNodes [0] .metadata) .not .toBe (null);
    expect (scene2 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
-   expect (scene2 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene2 .rootNodes [0] .metadata .value) .toHaveLength (6);
    expect (scene2 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+   expect (scene2 .rootNodes [0] .metadata .value [1] .getNodeTypeName ()) .toBe ("MetadataDouble");
+   expect (scene2 .rootNodes [0] .metadata .value [2] .getNodeTypeName ()) .toBe ("MetadataFloat");
+   expect (scene2 .rootNodes [0] .metadata .value [3] .getNodeTypeName ()) .toBe ("MetadataInteger");
+   expect (scene2 .rootNodes [0] .metadata .value [4] .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene2 .rootNodes [0] .metadata .value [5] .getNodeTypeName ()) .toBe ("MetadataString");
 });
 
 test ("containerField-4.0", async () =>
@@ -47,10 +60,13 @@ test ("containerField-4.0", async () =>
 <X3D profile='Core' version='4.0'>
    <Scene>
       <WorldInfo>
-         <MetadataSet containerField='metadata'
-               name='metadata'>
-            <MetadataBoolean
-                  name='value'/>
+         <MetadataSet containerField='metadata' name='metadata'>
+            <MetadataBoolean/>
+            <MetadataDouble/>
+            <MetadataFloat/>
+            <MetadataInteger/>
+            <MetadataSet/>
+            <MetadataString/>
          </MetadataSet>
       </WorldInfo>
    </Scene>
@@ -62,8 +78,13 @@ test ("containerField-4.0", async () =>
    expect (scene1 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
    expect (scene1 .rootNodes [0] .metadata) .not .toBe (null);
    expect (scene1 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
-   expect (scene1 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene1 .rootNodes [0] .metadata .value) .toHaveLength (6);
    expect (scene1 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+   expect (scene1 .rootNodes [0] .metadata .value [1] .getNodeTypeName ()) .toBe ("MetadataDouble");
+   expect (scene1 .rootNodes [0] .metadata .value [2] .getNodeTypeName ()) .toBe ("MetadataFloat");
+   expect (scene1 .rootNodes [0] .metadata .value [3] .getNodeTypeName ()) .toBe ("MetadataInteger");
+   expect (scene1 .rootNodes [0] .metadata .value [4] .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene1 .rootNodes [0] .metadata .value [5] .getNodeTypeName ()) .toBe ("MetadataString");
 
    const scene2 = await Browser .createX3DFromString (scene1 .toXMLString ());
 
@@ -72,6 +93,11 @@ test ("containerField-4.0", async () =>
    expect (scene2 .rootNodes [0] .getNodeTypeName ()) .toBe ("WorldInfo");
    expect (scene2 .rootNodes [0] .metadata) .not .toBe (null);
    expect (scene2 .rootNodes [0] .metadata .getNodeTypeName ()) .toBe ("MetadataSet");
-   expect (scene2 .rootNodes [0] .metadata .value) .toHaveLength (1);
+   expect (scene2 .rootNodes [0] .metadata .value) .toHaveLength (6);
    expect (scene2 .rootNodes [0] .metadata .value [0] .getNodeTypeName ()) .toBe ("MetadataBoolean");
+   expect (scene2 .rootNodes [0] .metadata .value [1] .getNodeTypeName ()) .toBe ("MetadataDouble");
+   expect (scene2 .rootNodes [0] .metadata .value [2] .getNodeTypeName ()) .toBe ("MetadataFloat");
+   expect (scene2 .rootNodes [0] .metadata .value [3] .getNodeTypeName ()) .toBe ("MetadataInteger");
+   expect (scene2 .rootNodes [0] .metadata .value [4] .getNodeTypeName ()) .toBe ("MetadataSet");
+   expect (scene2 .rootNodes [0] .metadata .value [5] .getNodeTypeName ()) .toBe ("MetadataString");
 });
