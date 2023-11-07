c145334ba000b231fe0a1deda9fa5d74ba9eca93
Holger Seelig <holger.seelig@gmail.com>
2023-11-07 11:50:45 +0100

    Added tests.

--- tests/standard/Utility/BitSet.test.js
+++ tests/standard/Utility/BitSet.test.js
@@ -51,74 +51,74 @@ test ("add", () =>
 {
    const b = new X3D .BitSet ();
 
-   b .add (0, 0x5);
+   b .add (0, 0xa);
 
-   expect (b .valueOf ()) .toBe (0x5);
+   expect (b .valueOf ()) .toBe (0x0000000a);
 
-   b .add (4, 0x5);
+   b .add (4, 0xb);
 
-   expect (b .valueOf ()) .toBe (0x55);
+   expect (b .valueOf ()) .toBe (0x000000ba);
 
-   b .add (8, 0x5);
+   b .add (8, 0xc);
 
-   expect (b .valueOf ()) .toBe (0x555);
+   expect (b .valueOf ()) .toBe (0x00000cba);
 
-   b .add (12, 0x5);
+   b .add (12, 0xd);
 
-   expect (b .valueOf ()) .toBe (0x5555);
+   expect (b .valueOf ()) .toBe (0x0000dcba);
 
-   b .add (16, 0x5);
+   b .add (16, 0xe);
 
-   expect (b .valueOf ()) .toBe (0x55555);
+   expect (b .valueOf ()) .toBe (0x000edcba);
 
-   b .add (20, 0x5);
+   b .add (20, 0xf);
 
-   expect (b .valueOf ()) .toBe (0x555555);
+   expect (b .valueOf ()) .toBe (0x00fedcba);
 
-   b .add (24, 0x5);
+   b .add (24, 0x9);
 
-   expect (b .valueOf ()) .toBe (0x5555555);
+   expect (b .valueOf ()) .toBe (0x09fedcba);
 
-   b .add (28, 0x5);
+   b .add (28, 0x8);
 
-   expect (b .valueOf ()) .toBe (0x55555555);
+   expect (b .valueOf ()) .toBe (0x89fedcba);
 });
 
 test ("remove", () =>
 {
-   const b = new X3D .BitSet (0x55555555);
+   const b = new X3D .BitSet (0xabcdef98);
 
-   expect (b .valueOf ()) .toBe (0x55555555);
+   expect (b .valueOf ()) .toBe (0xabcdef98);
 
-   b .remove (0, 0x5);
+   b .remove (0, 0x8);
 
-   expect (b .valueOf ()) .toBe (0x55555550);
+   expect (b .valueOf ()) .toBe (0xabcdef90);
 
-   b .remove (4, 0x5);
+   b .remove (4, 0x9);
 
-   expect (b .valueOf ()) .toBe (0x55555500);
+   expect (b .valueOf ()) .toBe (0xabcdef00);
 
-   b .remove (8, 0x5);
+   b .remove (8, 0xf);
 
-   expect (b .valueOf ()) .toBe (0x55555000);
+   expect (b .valueOf ()) .toBe (0xabcde000);
 
-   b .remove (12, 0x5);
+   b .remove (12, 0xe);
 
-   expect (b .valueOf ()) .toBe (0x55550000);
+   expect (b .valueOf ()) .toBe (0xabcd0000);
 
-   b .remove (16, 0x5);
+   b .remove (16, 0xd);
 
-   expect (b .valueOf ()) .toBe (0x55500000);
+   expect (b .valueOf ()) .toBe (0xabc00000);
 
-   b .remove (20, 0x5);
+   b .remove (20, 0xc);
 
-   expect (b .valueOf ()) .toBe (0x55000000);
+   expect (b .valueOf ()) .toBe (0xab000000);
 
-   b .remove (24, 0x5);
+   b .remove (24, 0xb);
 
-   expect (b .valueOf ()) .toBe (0x50000000);
+   expect (b .valueOf ()) .toBe (0xa0000000);
 
-   b .remove (28, 0x5);
+   b .remove (28, 0xa);
 
    expect (b .valueOf ()) .toBe (0);
 });
