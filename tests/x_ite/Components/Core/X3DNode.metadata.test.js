c6d23bd1461dc48ff5d0ca27947403496c52bdd9
Holger Seelig <holger.seelig@gmail.com>
2024-03-05 21:09:00 +0100

    Added tests.

--- tests/x_ite/Components/Core/X3DNode.metadata.test.js
+++ tests/x_ite/Components/Core/X3DNode.metadata.test.js
@@ -17,4 +17,28 @@ test ("get/setMetaData", () =>
    expect (node .metadata .value [0] .value) .toHaveLength (1);
    expect (node .metadata .value [0] .value [0] .name) .toBe ("boolean");
    expect (node .metadata .value [0] .value [0] .value .equals (new X3D .MFBool (true))) .toBe (true);
+
+   node .getValue () .setMetaData ("Sunrize/Test/double", 123.456);
+
+   expect (node .metadata) .toBeInstanceOf (X3D .SFNode);
+   expect (node .metadata .name) .toBe ("Sunrize");
+   expect (node .metadata .value) .toHaveLength (1);
+   expect (node .metadata .value [0] .name) .toBe ("Test");
+   expect (node .metadata .value [0] .value) .toHaveLength (2);
+   expect (node .metadata .value [0] .value [1] .name) .toBe ("double");
+   expect (node .metadata .value [0] .value [1] .value .equals (new X3D .MFDouble (123.456))) .toBe (true);
+
+   node .getValue () .removeMetaData ("Sunrize/Test/boolean");
+
+   expect (node .metadata) .toBeInstanceOf (X3D .SFNode);
+   expect (node .metadata .name) .toBe ("Sunrize");
+   expect (node .metadata .value) .toHaveLength (1);
+   expect (node .metadata .value [0] .name) .toBe ("Test");
+   expect (node .metadata .value [0] .value) .toHaveLength (1);
+   expect (node .metadata .value [0] .value [0] .name) .toBe ("double");
+   expect (node .metadata .value [0] .value [0] .value .equals (new X3D .MFDouble (123.456))) .toBe (true);
+
+   node .getValue () .removeMetaData ("Sunrize/Test/double", 123.456);
+
+   expect (node .metadata) .toBe (null);
 });
