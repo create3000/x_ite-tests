f5a3e4831761c49fd6f9381623d30f5e246c7318
Holger Seelig <holger.seelig@gmail.com>
2024-04-25 15:41:32 +0200

    Added tests.

--- tests/x_ite/Parser/OBJParser.test.js
+++ tests/x_ite/Parser/OBJParser.test.js
@@ -1,16 +1,27 @@
 const
    path = require ("path"),
-   url  = require ("url")
+   url  = require ("url");
 
-const X3D = require ("../../X3D")
+const X3D = require ("../../X3D");
 
 test ("LibertyStatue.obj.gz", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "OBJ", "LibertyStatue", `LibertStatue.obj.gz`))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "OBJ", "LibertyStatue", `LibertStatue.obj.gz`))));
 
-   expect (scene .encoding) .toBe ("OBJ")
-   expect (scene .rootNodes) .toHaveLength (1)
-})
+   expect (scene .encoding) .toBe ("OBJ");
+   expect (scene .rootNodes) .toHaveLength (1);
+});
+
+test ("LibertyStatue.obj.gz", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "OBJ", "minicooper.obj"))));
+
+   expect (scene .encoding) .toBe ("OBJ");
+   expect (scene .rootNodes) .toHaveLength (1);
+});
--- tests/x_ite/Parser/PLYAParser.test.js
+++ tests/x_ite/Parser/PLYAParser.test.js
@@ -1,16 +1,31 @@
 const
    path = require ("path"),
-   url  = require ("url")
+   url  = require ("url");
 
-const X3D = require ("../../X3D")
+const X3D = require ("../../X3D");
 
 test ("cube.ply", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "PLY", `cube.ply`))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "PLY", `cube.ply`))));
 
-   expect (scene .encoding) .toBe ("PLY")
-   expect (scene .rootNodes) .toHaveLength (2)
-})
+   expect (scene .encoding) .toBe ("PLY");
+   expect (scene .rootNodes) .toHaveLength (2);
+   expect (scene .rootNodes [1] .geometry .coordIndex) .toHaveLength (33);
+   expect (scene .rootNodes [1] .geometry .coord .point) .toHaveLength (8);
+});
+
+test ("cube32.ply", async () =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser,
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "PLY", `cube32.ply`))));
+
+   expect (scene .encoding) .toBe ("PLY");
+   expect (scene .rootNodes) .toHaveLength (2);
+   expect (scene .rootNodes [1] .geometry .coordIndex) .toHaveLength (30);
+   expect (scene .rootNodes [1] .geometry .coord .point) .toHaveLength (8);
+});
