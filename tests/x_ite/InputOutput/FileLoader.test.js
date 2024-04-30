6a7b6aa24de4cfd22e64607923ef1b23c184fe50
Holger Seelig <holger.seelig@gmail.com>
2024-04-30 21:59:29 +0200

    Added test.

--- tests/x_ite/InputOutput/FileLoader.test.js
+++ tests/x_ite/InputOutput/FileLoader.test.js
@@ -2,7 +2,7 @@ const
    X3D        = require ("../../X3D"),
    FileLoader = X3D .require ("x_ite/InputOutput/FileLoader")
 
-test ("data-url", () => new Promise ((resolve, reject) =>
+test ("data-url encoded", () => new Promise ((resolve, reject) =>
 {
    const
       canvas  = X3D .createBrowser (),
@@ -19,6 +19,36 @@ main ()
 `;
 
    new FileLoader (Browser .getWorld ()) .loadDocument (new X3D .MFString (encodeURI (`data:x-shader/x-vertex,${vertex}`)), function (data)
+   {
+      try
+      {
+         expect (data) .toBe (vertex)
+         resolve ()
+      }
+      catch (error)
+      {
+         reject (error .message)
+      }
+   });
+}))
+
+test ("data-url not encoded", () => new Promise ((resolve, reject) =>
+{
+   const
+      canvas  = X3D .createBrowser (),
+      Browser = canvas .browser
+
+   const vertex = `#version 300 es
+// Some percent % signs can cause to 100% an error.
+
+void
+main ()
+{
+   int i = 23 % 2; // Maybe as modulo.
+}
+`;
+
+   new FileLoader (Browser .getWorld ()) .loadDocument (new X3D .MFString (`data:x-shader/x-vertex,${vertex}`), function (data)
    {
       try
       {
