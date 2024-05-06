7c0e49a6f4334b3523752b73d95a38e08c0dc59b
Holger Seelig <holger.seelig@gmail.com>
2024-05-06 18:29:07 +0200

    Added test.

--- tests/x_ite/Browser/ConcreteNode.test.js
+++ tests/x_ite/Browser/ConcreteNode.test.js
@@ -4,8 +4,19 @@ const
    canvas  = X3D .createBrowser (),
    Browser = canvas .browser;
 
-test ("addConcreteNode", () =>
+test ("addConcreteNode", async () =>
 {
+   // Add custom component.
+
+   const Fun = new X3D .ComponentInfo ("Fun", 1, "Fun Component", "https://example.com");
+
+   Browser .addSupportedComponent (Fun);
+
+   expect (() => Browser .getSupportedComponent ("Fun")) .not .toThrow (Error);
+   expect (() => Browser .getComponent ("Fun", 1))       .not .toThrow (Error);
+
+   // Add custom concrete node.
+
    class Foo extends X3D .X3DNode
    {
       constructor (executionContext)
@@ -16,7 +27,7 @@ test ("addConcreteNode", () =>
       }
 
       static typeName           = "Foo";
-      static componentInfo      = Object .freeze ({ name: "Core", level: 1 });
+      static componentInfo      = Object .freeze ({ name: "Fun", level: 1 });
       static containerField     = "children";
       static specificationRange = Object .freeze ({ from: "2.0", to: "Infinity" });
       static fieldDefinitions   = new X3D .FieldDefinitionArray ([
@@ -34,4 +45,18 @@ test ("addConcreteNode", () =>
    expect (foo .getNodeTypeName ()) .toBe ("Foo");
    expect (foo .getNodeType () .includes (X3D .X3DConstants .Foo)) .toBe (true);
    expect (foo .bah) .toBe ("BAH");
+
+   // Test parse new node.
+
+   const scene = await Browser .createX3DFromString (`
+COMPONENT Fun : 1
+
+Foo {
+   bah "BIM"
+}
+   `);
+
+   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Foo");
+   expect (scene .rootNodes [0] .getNodeType () .includes (X3D .X3DConstants .Foo)) .toBe (true);
+   expect (scene .rootNodes [0] .bah) .toBe ("BIM");
 });
