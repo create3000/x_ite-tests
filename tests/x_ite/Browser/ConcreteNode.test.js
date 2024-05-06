d7484c67cf4c07afeb77055c0fc5154b6d208989
Holger Seelig <holger.seelig@gmail.com>
2024-05-06 18:06:39 +0200

    Added test.

---
+++ tests/x_ite/Browser/ConcreteNode.test.js
@@ -0,0 +1,34 @@
+
+const
+   X3D     = require ("../../X3D"),
+   canvas  = X3D .createBrowser (),
+   Browser = canvas .browser;
+
+test ("addConcreteNode", () =>
+{
+   class Foo extends X3D .X3DNode
+   {
+      constructor (executionContext)
+      {
+         super (executionContext);
+
+         this .addType (X3D .X3DConstants .Foo);
+      }
+
+      static typeName           = "Foo";
+      static componentInfo      = Object .freeze ({ name: "Core", level: 1 });
+      static containerField     = "children";
+      static specificationRange = Object .freeze ({ from: "2.0", to: "Infinity" });
+      static fieldDefinitions   = new X3D .FieldDefinitionArray ([
+         new X3D .X3DFieldDefinition (X3D .X3DConstants .inputOutput, "metadata", new X3D .SFNode ()),
+         new X3D .X3DFieldDefinition (X3D .X3DConstants .inputOutput, "bah",      new X3D .SFString ("BAH")),
+      ]);
+   }
+
+   Browser .addConcreteNode (Foo);
+
+   const foo = Browser .currentScene .createNode ("Foo");
+
+   expect (foo .getNodeTypeName ()) .toBe ("Foo");
+   expect (foo .bah) .toBe ("BAH");
+});
