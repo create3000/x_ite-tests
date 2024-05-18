e4e4acce149ad8d1f0578ecd281902c64ac3b418
Holger Seelig <holger.seelig@gmail.com>
2024-05-18 17:59:07 +0200

    Added tests.

--- tests/x_ite/Parser/GoldenGate.test.js
+++ tests/x_ite/Parser/GoldenGate.test.js
@@ -5,12 +5,15 @@ test ("add/removeParser", () =>
    const originalParsers = X3D .GoldenGate .getParsers ();
 
    // Legacy
+   expect (X3D .GoldenGate .Parser) .toBe (X3D .GoldenGate .Parser);
    expect (X3D .GoldenGate .Parser) .toEqual (X3D .GoldenGate .getParsers ());
 
    const newParsers = [function (scene) { }, function (scene) { }];
 
    X3D .GoldenGate .addParsers (... newParsers);
 
+   expect (X3D .GoldenGate .getParsers ()) .not .toBe (X3D .GoldenGate .getParsers ());
+   expect (X3D .GoldenGate .getParsers ()) .toEqual (X3D .GoldenGate .getParsers ());
    expect (X3D .GoldenGate .getParsers ()) .toHaveLength (originalParsers .length + 2);
 
    X3D .GoldenGate .removeParsers (... newParsers);
