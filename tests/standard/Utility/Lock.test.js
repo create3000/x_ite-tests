c1e8fb8d63ec7311f6f8b568d179437bccba22fb
Holger Seelig <holger.seelig@gmail.com>
2024-08-16 20:10:27 +0200

    Added test.

--- tests/standard/Utility/Lock.test.js
+++ tests/standard/Utility/Lock.test.js
@@ -8,7 +8,7 @@ test ("constructor", async () =>
 {
    const r = [ ];
 
-   function f (ms, v)
+   function a (ms, v)
    {
       return Lock .acquire ("Lock.test 1", async () =>
       {
@@ -18,16 +18,22 @@ test ("constructor", async () =>
 
          r .push (v);
 
+         if (v === 222)
+            throw new Error ();
+
          return v;
       });
    }
 
+   const f = [ ];
+
    await Promise .all ([
-      f (200, 111) .then (v => expect (v) .toBe (111)),
-      f (100, 222) .then (v => expect (v) .toBe (222)),
-      f (200, 333) .then (v => expect (v) .toBe (333)),
-      f (100, 444) .then (v => expect (v) .toBe (444)),
+      a (200, 111) .then (v => expect (v) .toBe (111)) .finally (() => f .push (111)),
+      a (100, 222) .catch (e => expect (e) .toBeInstanceOf (Error)) .finally (() => f .push (222)),
+      a (200, 333) .then (v => expect (v) .toBe (333)) .finally (() => f .push (333)),
+      a (100, 444) .then (v => expect (v) .toBe (444)) .finally (() => f .push (444)),
    ]);
 
    expect (r) .toEqual ([111, 111, 222, 222, 333, 333, 444, 444]);
+   expect (f) .toEqual ([111, 222, 333, 444]);
 });
