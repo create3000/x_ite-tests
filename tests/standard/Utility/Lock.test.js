639bbe7810486e11c700cdee93bdaf39f8fe7427
Holger Seelig <holger.seelig@gmail.com>
2024-08-16 20:16:06 +0200

    Updated test.

--- tests/standard/Utility/Lock.test.js
+++ tests/standard/Utility/Lock.test.js
@@ -8,7 +8,7 @@ test ("constructor", async () =>
 {
    const r = [ ];
 
-   function a (ms, v)
+   function a (ms, v, t)
    {
       return Lock .acquire ("Lock.test 1", async () =>
       {
@@ -18,22 +18,26 @@ test ("constructor", async () =>
 
          r .push (v);
 
-         if (v === 222)
+         if (t)
             throw new Error ();
 
          return v;
       });
    }
 
+   const t = [ ];
+   const c = [ ];
    const f = [ ];
 
    await Promise .all ([
-      a (200, 111) .then (v => expect (v) .toBe (111)) .finally (() => f .push (111)),
-      a (100, 222) .catch (e => expect (e) .toBeInstanceOf (Error)) .finally (() => f .push (222)),
-      a (200, 333) .then (v => expect (v) .toBe (333)) .finally (() => f .push (333)),
-      a (100, 444) .then (v => expect (v) .toBe (444)) .finally (() => f .push (444)),
+      a (200, 111) .then (v => t .push (v)) .finally (() => f .push (111)),
+      a (100, 222, true) .catch (e => c .push (e)) .finally (() => f .push (222)),
+      a (200, 333) .then (v => t .push (v)) .finally (() => f .push (333)),
+      a (100, 444) .then (v => t .push (v)) .finally (() => f .push (444)),
    ]);
 
    expect (r) .toEqual ([111, 111, 222, 222, 333, 333, 444, 444]);
+   expect (t) .toEqual ([111, 333, 444]);
+   expect (c [0]) .toBeInstanceOf (Error);
    expect (f) .toEqual ([111, 222, 333, 444]);
 });
