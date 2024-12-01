7be4c7833e7c2df33fab265a442d0d683c2b5b72
Holger Seelig <holger.seelig@gmail.com>
2024-12-01 21:29:46 +0100

    Use clamp again.

--- tests/x_ite/Parser/SVGParser.test.js
+++ tests/x_ite/Parser/SVGParser.test.js
@@ -1,27 +1,27 @@
 const
    path = require ("path"),
-   url  = require ("url")
+   url  = require ("url");
 
-const X3D = require ("../../X3D")
+const X3D = require ("../../X3D");
 
 test ("Primitives.svg", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "SVG", `Primitives.svg`))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "SVG", `Primitives.svg`))));
 
-   expect (scene .encoding) .toBe ("SVG")
-   expect (scene .rootNodes) .toHaveLength (4)
-})
+   expect (scene .encoding) .toBe ("SVG");
+   expect (scene .rootNodes) .toHaveLength (3);
+});
 
 test ("design.svg", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "SVG", `design.svg`))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "SVG", `design.svg`))));
 
-   expect (scene .encoding) .toBe ("SVG")
-   expect (scene .rootNodes) .toHaveLength (4)
-})
+   expect (scene .encoding) .toBe ("SVG");
+   expect (scene .rootNodes) .toHaveLength (3);
+});
