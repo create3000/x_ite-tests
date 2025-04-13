8d2da13b1a32138474b7293aadf563a0efa9d8d7
Holger Seelig <holger.seelig@gmail.com>
2025-04-13 12:57:00 +0200

    Add TimeSensor test for timeout functionality.

---
+++ tests/x_ite/Components/Time/TimeSensor.test.js
@@ -0,0 +1,23 @@
+const
+   X3D     = require ("../../../X3D"),
+   Browser = X3D .createBrowser () .browser;
+
+test ("timeOut 1s", () => new Promise (async resolve =>
+{
+   const
+      scene    = await Browser .createScene (Browser .getProfile ("Interactive")),
+      timer    = scene .createNode ("TimeSensor"),
+      duration = 1,
+      t0       = Date .now () / 1000;
+
+   timer .startTime = t0 + duration;
+
+   timer .addFieldCallback ("test", "isActive", value =>
+   {
+      if (!value)
+         return;
+
+      expect (Date .now () / 1000 >= t0 + duration) .toBe (true);
+      resolve ();
+   });
+}));
