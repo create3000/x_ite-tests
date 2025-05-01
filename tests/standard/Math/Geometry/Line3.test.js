e9e961cc090b0e4e37b65378c4600a0e47872acb
Holger Seelig <holger.seelig@gmail.com>
2025-05-01 13:05:25 +0200

    Added multLineMatrix tests.

--- tests/standard/Math/Geometry/Line3.test.js
+++ tests/standard/Math/Geometry/Line3.test.js
@@ -112,3 +112,16 @@ test ("setPoints", () =>
 
    expect (l1 .direction .equals (d1)) .toBe (true);
 });
+
+test ("multLineMatrix", () =>
+{
+   const p1 = Vector3 .Zero;
+   const d1 = Vector3 .xAxis;
+   const l1 = new Line3 (p1, d1);
+   const m1 = new Matrix4 (0, 1, 0, 0,  -1, 0, 0, 0,  0, 0, 1, 0,  1, 1, 1, -1);
+
+   l1 .multLineMatrix (m1);
+
+   expect (l1 .point .equals (Vector3 .One .copy () .negate ())) .toBe (true);
+   expect (l1 .direction .equals (Vector3 .yAxis)) .toBe (true);
+});
