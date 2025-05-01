3ff809824d2052975e161335832eba68a1bdda3a
Holger Seelig <holger.seelig@gmail.com>
2025-05-01 15:38:40 +0200

    Added constructor tests.

--- tests/standard/Math/Geometry/Plane3.test.js
+++ tests/standard/Math/Geometry/Plane3.test.js
@@ -11,4 +11,19 @@ test ("constructor", () =>
 
    expect (p1 .normal .equals (Vector3 .zAxis)) .toBe (true);
    expect (p1 .distanceFromOrigin) .toBe (0);
+
+   const px = new Plane3 (Vector3 .xAxis, Vector3 .xAxis);
+
+   expect (px .normal .equals (Vector3 .xAxis)) .toBe (true);
+   expect (px .distanceFromOrigin) .toBe (1);
+
+   const py = new Plane3 (Vector3 .yAxis, Vector3 .yAxis);
+
+   expect (py .normal .equals (Vector3 .yAxis)) .toBe (true);
+   expect (py .distanceFromOrigin) .toBe (1);
+
+   const pz = new Plane3 (Vector3 .zAxis, Vector3 .zAxis);
+
+   expect (pz .normal .equals (Vector3 .zAxis)) .toBe (true);
+   expect (pz .distanceFromOrigin) .toBe (1);
 });
