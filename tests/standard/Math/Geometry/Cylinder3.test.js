2066a3fc76dcc1978e096a2b6821185a6ddd5853
Holger Seelig <holger.seelig@gmail.com>
2025-05-02 14:23:20 +0200

    Added intersectsLine tests.

--- tests/standard/Math/Geometry/Cylinder3.test.js
+++ tests/standard/Math/Geometry/Cylinder3.test.js
@@ -73,6 +73,55 @@ test ("set", () =>
 
 test ("intersectsLine", () =>
 {
+   const en = new Vector3 ();
+   const ex = new Vector3 ();
+
+   const c1 = new Cylinder3 (new Line3 (Vector3 .Zero, Vector3 .xAxis), 2);
+   const lx = new Line3 (Vector3 .Zero, Vector3 .yAxis);
+
+   expect (c1 .intersectsLine (lx, en, ex)) .toBe (true);
+   expect (en .x) .toBeCloseTo (0);
+   expect (en .y) .toBeCloseTo (-2);
+   expect (en .z) .toBeCloseTo (0);
+   expect (ex .x) .toBeCloseTo (0);
+   expect (ex .y) .toBeCloseTo (2);
+   expect (ex .z) .toBeCloseTo (0);
+
+   const c2 = new Cylinder3 (new Line3 (Vector3 .Zero, Vector3 .yAxis), 2);
+   const ly = new Line3 (Vector3 .Zero, Vector3 .zAxis);
+
+   expect (c2 .intersectsLine (ly, en, ex)) .toBe (true);
+   expect (en .x) .toBeCloseTo (0);
+   expect (en .y) .toBeCloseTo (0);
+   expect (en .z) .toBeCloseTo (-2);
+   expect (ex .x) .toBeCloseTo (0);
+   expect (ex .y) .toBeCloseTo (0);
+   expect (ex .z) .toBeCloseTo (2);
+
+   const c3 = new Cylinder3 (new Line3 (Vector3 .Zero, Vector3 .zAxis), 2);
+   const lz = new Line3 (Vector3 .Zero, Vector3 .xAxis);
+
+   expect (c3 .intersectsLine (lz, en, ex)) .toBe (true);
+   expect (en .x) .toBeCloseTo (-2);
+   expect (en .y) .toBeCloseTo (0);
+   expect (en .z) .toBeCloseTo (0);
+   expect (ex .x) .toBeCloseTo (2);
+   expect (ex .y) .toBeCloseTo (0);
+   expect (ex .z) .toBeCloseTo (0);
+
+   // Not precise enough for this tests:
+
+   // const ox = new Line3 (new Vector3 (0, 4, 0), Vector3 .xAxis);
+
+   // expect (c1 .intersectsLine (ox, en, ex)) .toBe (true);
+
+   // const oy = new Line3 (new Vector3 (0, 2, 4), Vector3 .yAxis);
+
+   // expect (c1 .intersectsLine (oy, en, ex)) .toBe (false);
+
+   // const oz = new Line3 (new Vector3 (4, 0, 0), Vector3 .zAxis);
+
+   // expect (c1 .intersectsLine (oz, en, ex)) .toBe (false);
 });
 
 test ("toString", () =>
