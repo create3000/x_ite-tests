237919eef09cc539e1ecf05b8ab3d423012b76d8
Holger Seelig <holger.seelig@gmail.com>
2025-05-03 12:12:21 +0200

    Test Triangle3.

--- tests/standard/Math/Geometry/Triangle3.test.js
+++ tests/standard/Math/Geometry/Triangle3.test.js
@@ -32,3 +32,30 @@ test ("quadNormal", () =>
 
    expect (Triangle3 .quadNormal (a, b, c, d, n) .equals (Vector3 .zAxis)) .toBe (true);
 });
+
+test ("triangulatePolygon", () =>
+{
+   const a = new Vector3 ();
+   const b = new Vector3 (1, 0, 0);
+   const c = new Vector3 (1, 1, 0);
+   const d = new Vector3 (0, 1, 0);
+
+   const polygon = [ a, b, c, d ];
+
+   polygon .forEach ((v, i) => v .index = i);
+
+   const triangles = Triangle3 .triangulatePolygon (polygon, [ ]);
+
+   expect (triangles) .toEqual ([1, 3, 0, 3, 1, 2]);
+});
+
+test ("triangulateConvexPolygon", () =>
+{
+   const polygon = [ 0, 1, 2, 3 ];
+
+   polygon .forEach ((v, i) => v .index = i);
+
+   const triangles = Triangle3 .triangulateConvexPolygon (polygon, [ ]);
+
+   expect (triangles) .toEqual ([0, 1, 2,  0, 2, 3]);
+});
