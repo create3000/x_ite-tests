76e2f4280cd536705c1f4dd14f4e809e6c41c851
Holger Seelig <holger.seelig@gmail.com>
2025-07-14 10:34:07 +0200

    Test initial value_changed.

---
+++ tests/x_ite/Components/Interpolation/CoordinateInterpolator2D.test.js
@@ -0,0 +1,24 @@
+
+
+const X3D = require ("../../../X3D");
+
+const
+   canvas  = X3D .createBrowser (),
+   Browser = canvas .browser;
+
+test ("initial value_changed", async () =>
+{
+   const scene = await Browser .createX3DFromString (`#X3D V4.0 utf8
+
+PROFILE Interchange
+COMPONENT Interpolation : 5
+
+CoordinateInterpolator2D {
+   key [0, 1]
+   keyValue [5 5, 6 6, 7 7, 8 8]
+}
+   `);
+
+   expect (scene .rootNodes) .toHaveLength (1);
+   expect (scene .rootNodes [0] .value_changed .equals (new X3D .MFVec2f (new X3D .SFVec2f(5,5), new X3D .SFVec2f(6,6)))) .toBe (true);
+});
