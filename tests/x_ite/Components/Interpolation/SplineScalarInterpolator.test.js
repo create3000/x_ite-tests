0baf290de407535b9ef217468975455c5c56c99b
Holger Seelig <holger.seelig@gmail.com>
2025-07-14 11:23:35 +0200

    Test initial value_changed.

---
+++ tests/x_ite/Components/Interpolation/SplineScalarInterpolator.test.js
@@ -0,0 +1,24 @@
+
+
+const X3D = require ("../../../X3D");
+
+const
+   canvas  = X3D .createBrowser (),
+   Browser = canvas .browser;
+
+test ("initial value_changed", async () =>
+{
+   const scene = await Browser .createX3DFromString (`#X3D V4.0 utf8
+
+PROFILE Interchange
+COMPONENT Interpolation : 5
+
+SplineScalarInterpolator {
+   key [0, 1]
+   keyValue [5, 6]
+}
+   `);
+
+   expect (scene .rootNodes) .toHaveLength (1);
+   expect (scene .rootNodes [0] .value_changed) .toBe (5);
+});
---
+++ tests/x_ite/Components/Interpolation/SquadOrientationInterpolator.test.js
@@ -0,0 +1,24 @@
+
+
+const X3D = require ("../../../X3D");
+
+const
+   canvas  = X3D .createBrowser (),
+   Browser = canvas .browser;
+
+test ("initial value_changed", async () =>
+{
+   const scene = await Browser .createX3DFromString (`#X3D V4.0 utf8
+
+PROFILE Interchange
+COMPONENT Interpolation : 5
+
+SquadOrientationInterpolator {
+   key [0, 1]
+   keyValue [5 5 5 5, 6 6 6 6]
+}
+   `);
+
+   expect (scene .rootNodes) .toHaveLength (1);
+   expect (scene .rootNodes [0] .value_changed .equals (new X3D .SFRotation (5,5,5,5))) .toBe (true);
+});
