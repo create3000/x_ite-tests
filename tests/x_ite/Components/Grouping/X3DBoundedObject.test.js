7dd3127fa0bf39d4236eca428e5afa42655dc057
Holger Seelig <holger.seelig@gmail.com>
2025-08-05 16:39:02 +0200

    Check custom bbox.

--- tests/x_ite/Components/Grouping/X3DBoundedObject.test.js
+++ tests/x_ite/Components/Grouping/X3DBoundedObject.test.js
@@ -17,6 +17,20 @@ test ("getBBox", async () =>
 
       console .log (node .getNodeTypeName ());
 
+      // Check default bbox.
+
       expect (() => node .getValue () .getBBox (new X3D .Box3 ())) .not .toThrow (Error);
+
+      // Check custom bbox.
+
+      node .bboxSize   = new X3D .SFVec3f (2, 3, 4);
+      node .bboxCenter = new X3D .SFVec3f (5, 6, 7);
+
+      await Browser .nextFrame ();
+
+      const bbox = node .getValue () .getBBox (new X3D .Box3 ());
+
+      expect (bbox .size   .equals (new X3D .Vector3 (2, 3, 4))) .toBe (true);
+      expect (bbox .center .equals (new X3D .Vector3 (5, 6, 7))) .toBe (true);
    }
 });
