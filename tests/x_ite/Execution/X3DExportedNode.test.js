48a1ef90822431c35539f52f459460dfb90b9b8d
Holger Seelig <holger.seelig@gmail.com>
2025-08-17 10:33:55 +0200

    Updated import/export test.

--- tests/x_ite/Execution/X3DExportedNode.test.js
+++ tests/x_ite/Execution/X3DExportedNode.test.js
@@ -13,34 +13,36 @@ DEF N1 Group { }
 DEF N2 Switch { }
 
 EXPORT N1 AS E1
-EXPORT N2 AS E2
+EXPORT N2 AS E2 DESCRIPTION "Test Desc"
    `)
 
    await sleep ();
 
-   const exportedNode = scene .exportedNodes [0]
+   const exportedNode0 = scene .exportedNodes [0]
 
-   expect (exportedNode) .toBeInstanceOf (X3D .X3DExportedNode)
-   expect (exportedNode .constructor) .toBe (X3D .X3DExportedNode)
-   expect (exportedNode .exportedName) .toBe ("E1")
-   expect (exportedNode .localNode) .toBeInstanceOf (X3D .SFNode)
-   expect (exportedNode .localNode) .toBe (scene .getNamedNode ("N1"))
-   expect (exportedNode .getExportedName ()) .toBe (exportedNode .exportedName)
-   expect (exportedNode .getLocalNode ()) .toBe (exportedNode .localNode .getValue ())
-   expect (exportedNode .toString ()) .toBe (`[object ${exportedNode .getTypeName ()}]`)
+   expect (exportedNode0) .toBeInstanceOf (X3D .X3DExportedNode)
+   expect (exportedNode0 .constructor) .toBe (X3D .X3DExportedNode)
+   expect (exportedNode0 .exportedName) .toBe ("E1")
+   expect (exportedNode0 .localNode) .toBeInstanceOf (X3D .SFNode)
+   expect (exportedNode0 .localNode) .toBe (scene .getNamedNode ("N1"))
+   expect (exportedNode0 .description) .toBe ("")
+   expect (exportedNode0 .getExportedName ()) .toBe (exportedNode0 .exportedName)
+   expect (exportedNode0 .getLocalNode ()) .toBe (exportedNode0 .localNode .getValue ())
+   expect (exportedNode0 .getDescription ()) .toBe ("")
+   expect (exportedNode0 .toString ()) .toBe (`[object ${exportedNode0 .getTypeName ()}]`)
 
-   exportedNode .exportedName = undefined
-   exportedNode .localNode    = undefined
+   exportedNode0 .exportedName = undefined
+   exportedNode0 .localNode    = undefined
 
-   expect (exportedNode) .toBeInstanceOf (X3D .X3DExportedNode)
-   expect (exportedNode .exportedName) .toBe ("E1")
-   expect (exportedNode .localNode) .toBeInstanceOf (X3D .SFNode)
-   expect (exportedNode .localNode) .toBe (scene .getNamedNode ("N1"))
+   expect (exportedNode0) .toBeInstanceOf (X3D .X3DExportedNode)
+   expect (exportedNode0 .exportedName) .toBe ("E1")
+   expect (exportedNode0 .localNode) .toBeInstanceOf (X3D .SFNode)
+   expect (exportedNode0 .localNode) .toBe (scene .getNamedNode ("N1"))
 
    expect (X3D .X3DExportedNode .typeName) .toBe ("X3DExportedNode")
-   expect (exportedNode .getTypeName ()) .toBe ("X3DExportedNode")
-   expect (Object .prototype .toString .call (exportedNode)) .toBe (`[object X3DExportedNode]`)
-   expect (exportedNode .toString ()) .toBe (`[object ${exportedNode .getTypeName ()}]`)
+   expect (exportedNode0 .getTypeName ()) .toBe ("X3DExportedNode")
+   expect (Object .prototype .toString .call (exportedNode0)) .toBe (`[object X3DExportedNode]`)
+   expect (exportedNode0 .toString ()) .toBe (`[object ${exportedNode0 .getTypeName ()}]`)
 
    const properties = [
       "exportedName",
@@ -48,5 +50,18 @@ EXPORT N2 AS E2
       "description",
    ]
 
-   enumerate (properties, exportedNode)
+   enumerate (properties, exportedNode0);
+
+   const exportedNode1 = scene .exportedNodes [1]
+
+   expect (exportedNode1) .toBeInstanceOf (X3D .X3DExportedNode)
+   expect (exportedNode1 .constructor) .toBe (X3D .X3DExportedNode)
+   expect (exportedNode1 .exportedName) .toBe ("E2")
+   expect (exportedNode1 .localNode) .toBeInstanceOf (X3D .SFNode)
+   expect (exportedNode1 .localNode) .toBe (scene .getNamedNode ("N2"))
+   expect (exportedNode1 .description) .toBe ("Test Desc")
+   expect (exportedNode1 .getExportedName ()) .toBe (exportedNode1 .exportedName)
+   expect (exportedNode1 .getLocalNode ()) .toBe (exportedNode1 .localNode .getValue ())
+   expect (exportedNode1 .getDescription ()) .toBe ("Test Desc")
+   expect (exportedNode1 .toString ()) .toBe (`[object ${exportedNode1 .getTypeName ()}]`)
 })
