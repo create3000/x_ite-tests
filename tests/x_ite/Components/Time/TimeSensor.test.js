1e6da302516a28f71ba1643fc5ce692f06aabc8f
Holger Seelig <holger.seelig@gmail.com>
2025-09-11 23:21:17 +0200

    Added test.

--- tests/x_ite/Components/Time/TimeSensor.test.js
+++ tests/x_ite/Components/Time/TimeSensor.test.js
@@ -5,6 +5,40 @@ const
 Browser .setBrowserOption ("AutoUpdate", false);
 Browser .beginUpdate ();
 
+test ("cycleTime", () => new Promise (async resolve =>
+{
+   const
+      scene = await Browser .createScene (Browser .getProfile ("Interactive")),
+      timer = scene .createNode ("TimeSensor");
+
+   let
+      fraction = 0,
+      cycles   = 0;
+
+   timer .addFieldCallback ("test", "cycleTime", value =>
+   {
+      ++ cycles;
+
+      if (cycles === 1)
+         expect (fraction) .toBe (0);
+      else
+         expect (fraction) .toBeGreaterThan (0.6);
+
+      if (cycles > 3)
+         resolve ();
+   });
+
+   timer .addFieldCallback ("test", "fraction_changed", value =>
+   {
+      fraction = value;
+   });
+
+   timer .cycleInterval = 0.25;
+   timer .loop          = true;
+   timer .startTime     = Date .now () / 1000;
+}),
+2000);
+
 test ("timeOut 1s", () => new Promise (async resolve =>
 {
    const
