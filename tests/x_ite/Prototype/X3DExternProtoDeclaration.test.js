2c9ee7b81c599721f31c979b5ad9088b82310d66
Holger Seelig <holger.seelig@gmail.com>
2025-10-15 08:06:53 +0200

    Test isInitialized.

--- tests/x_ite/Prototype/X3DExternProtoDeclaration.test.js
+++ tests/x_ite/Prototype/X3DExternProtoDeclaration.test.js
@@ -1,13 +1,13 @@
 
 const
    path = require ("path"),
-   url  = require ("url")
+   url  = require ("url");
 
 const X3D = require ("../../X3D")
 
 const
    canvas  = X3D .createBrowser (),
-   Browser = canvas .browser
+   Browser = canvas .browser;
 
 test ("properties", async () =>
 {
@@ -48,75 +48,77 @@ Test { size 4 4 4 }
    expect (scene .rootNodes [0] .size .equals (new X3D .SFVec3f (2,2,2))) .toBe (true);
    expect (scene .rootNodes [1] .size .equals (new X3D .SFVec3f (4,4,4))) .toBe (true);
 
-   expect (scene .externprotos) .toHaveLength (1)
+   expect (scene .externprotos) .toHaveLength (1);
 
-   const externproto = scene .externprotos [0]
+   const externproto = scene .externprotos [0];
 
-   expect (externproto) .toBeInstanceOf (X3D .X3DExternProtoDeclaration)
-   expect (externproto .constructor) .toBe (X3D .X3DExternProtoDeclaration)
-   expect (externproto .name) .toBe ("Test")
-   expect (externproto .loadState) .toBe (X3D .X3DConstants .COMPLETE_STATE)
-   expect (externproto .isExternProto) .toBe (true)
-   expect (externproto .urls) .toBeInstanceOf (X3D .MFString)
-   expect (externproto .appInfo) .toBe ("")
-   expect (externproto .documentation) .toBe ("")
-   expect (externproto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
-   expect (externproto .fields) .toHaveLength (2)
-   expect (externproto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (externproto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
-   expect (externproto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
-   expect (externproto .fields [0] .name) .toBe ("metadata")
-   expect (externproto .fields [0] .value) .toBe (null)
-   expect (externproto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (externproto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
-   expect (externproto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
-   expect (externproto .fields [1] .name) .toBe ("size")
-   expect (externproto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f)
-   expect (externproto .getName ()) .toBe (externproto .name)
+   expect (externproto) .toBeInstanceOf (X3D .X3DExternProtoDeclaration);
+   expect (externproto .constructor) .toBe (X3D .X3DExternProtoDeclaration);
+   expect (externproto .name) .toBe ("Test");
+   expect (externproto .loadState) .toBe (X3D .X3DConstants .COMPLETE_STATE);
+   expect (externproto .isExternProto) .toBe (true);
+   expect (externproto .urls) .toBeInstanceOf (X3D .MFString);
+   expect (externproto .appInfo) .toBe ("");
+   expect (externproto .documentation) .toBe ("");
+   expect (externproto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray);
+   expect (externproto .fields) .toHaveLength (2);
+   expect (externproto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (externproto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput);
+   expect (externproto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode);
+   expect (externproto .fields [0] .name) .toBe ("metadata");
+   expect (externproto .fields [0] .value) .toBe (null);
+   expect (externproto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (externproto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly);
+   expect (externproto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f);
+   expect (externproto .fields [1] .name) .toBe ("size");
+   expect (externproto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f);
+   expect (externproto .getName ()) .toBe (externproto .name);
 
-   expect (X3D .X3DExternProtoDeclaration .typeName) .toBe ("X3DExternProtoDeclaration")
-   expect (externproto .getTypeName ()) .toBe ("X3DExternProtoDeclaration")
-   expect (Object .prototype .toString .call (externproto)) .toBe (`[object X3DExternProtoDeclaration]`)
-   expect (externproto .toString ()) .toBe (`[object ${externproto .getTypeName ()}]`)
+   expect (X3D .X3DExternProtoDeclaration .typeName) .toBe ("X3DExternProtoDeclaration");
+   expect (externproto .getTypeName ()) .toBe ("X3DExternProtoDeclaration");
+   expect (Object .prototype .toString .call (externproto)) .toBe (`[object X3DExternProtoDeclaration]`);
+   expect (externproto .toString ()) .toBe (`[object ${externproto .getTypeName ()}]`);
 
-   externproto .name = undefined
-   externproto .loadState = undefined
-   externproto .isExternProto = undefined
-   externproto .urls = undefined
-   externproto .appInfo = undefined
-   externproto .documentation = undefined
-   externproto .fields = undefined
+   externproto .name = undefined;
+   externproto .loadState = undefined;
+   externproto .isExternProto = undefined;
+   externproto .urls = undefined;
+   externproto .appInfo = undefined;
+   externproto .documentation = undefined;
+   externproto .fields = undefined;
 
-   expect (externproto .name) .toBe ("Test")
-   expect (externproto .loadState) .toBe (X3D .X3DConstants .COMPLETE_STATE)
-   expect (externproto .isExternProto) .toBe (true)
-   expect (externproto .urls) .toBeInstanceOf (X3D .MFString)
-   expect (externproto .appInfo) .toBe ("")
-   expect (externproto .documentation) .toBe ("")
-   expect (externproto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
-   expect (externproto .fields) .toHaveLength (2)
-   expect (externproto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (externproto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
-   expect (externproto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
-   expect (externproto .fields [0] .name) .toBe ("metadata")
-   expect (externproto .fields [0] .value) .toBe (null)
-   expect (externproto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (externproto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
-   expect (externproto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
-   expect (externproto .fields [1] .name) .toBe ("size")
-   expect (externproto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f)
+   expect (externproto .name) .toBe ("Test");
+   expect (externproto .loadState) .toBe (X3D .X3DConstants .COMPLETE_STATE);
+   expect (externproto .isExternProto) .toBe (true);
+   expect (externproto .urls) .toBeInstanceOf (X3D .MFString);
+   expect (externproto .appInfo) .toBe ("");
+   expect (externproto .documentation) .toBe ("");
+   expect (externproto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray);
+   expect (externproto .fields) .toHaveLength (2);
+   expect (externproto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (externproto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput);
+   expect (externproto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode);
+   expect (externproto .fields [0] .name) .toBe ("metadata");
+   expect (externproto .fields [0] .value) .toBe (null);
+   expect (externproto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (externproto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly);
+   expect (externproto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f);
+   expect (externproto .fields [1] .name) .toBe ("size");
+   expect (externproto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f);
 
-   const instance = externproto .newInstance ()
+   const instance = externproto .newInstance ();
 
-   expect (instance) .toBeInstanceOf (X3D .SFNode)
-   expect (instance .getNodeTypeName ()) .toBe ("Test")
+   expect (instance) .toBeInstanceOf (X3D .SFNode);
+   expect (instance .getValue ()) .toBeInstanceOf (X3D .X3DPrototypeInstance);
+   expect (instance .getValue () .isInitialized ()) .toBe (true);
+   expect (instance .getNodeTypeName ()) .toBe ("Test");
 
-   const body = instance .getValue () .getBody ()
+   const body = instance .getValue () .getBody ();
 
-   expect (body .rootNodes) .toHaveLength (2)
-   expect (body .protos) .toHaveLength (0)
-   expect (body .externprotos) .toHaveLength (0)
-   expect (body .routes) .toHaveLength (1)
+   expect (body .rootNodes) .toHaveLength (2);
+   expect (body .protos) .toHaveLength (0);
+   expect (body .externprotos) .toHaveLength (0);
+   expect (body .routes) .toHaveLength (1);
 
    const properties = [
       "name",
@@ -126,19 +128,19 @@ Test { size 4 4 4 }
       "fields",
       "appInfo",
       "documentation",
-   ]
+   ];
 
-   enumerate (properties, externproto)
-})
+   enumerate (properties, externproto);
+});
 
 test ("toString", () =>
 {
    const
       scene  = Browser .currentScene,
-      protos = scene .protos
+      protos = scene .protos;
 
-   expect (protos .toString ()) .toBe (`[object ${protos .getTypeName ()}]`)
-})
+   expect (protos .toString ()) .toBe (`[object ${protos .getTypeName ()}]`);
+});
 
 test ("load-not-started", async () =>
 {
@@ -149,12 +151,12 @@ EXTERNPROTO Test [
    inputOutput SFBool test
 ]
 "${url .pathToFileURL (path .join (__dirname, "files", "proto.x3d"))}"
-`)
+`);
 
-   expect (scene .rootNodes) .toHaveLength (0)
-   expect (scene .externprotos [0] .loadState) .toBe (X3D .X3DConstants .NOT_STARTED_STATE)
-   expect (scene .externprotos [0] .checkLoadState ()) .toBe (X3D .X3DConstants .NOT_STARTED_STATE)
-})
+   expect (scene .rootNodes) .toHaveLength (0);
+   expect (scene .externprotos [0] .loadState) .toBe (X3D .X3DConstants .NOT_STARTED_STATE);
+   expect (scene .externprotos [0] .checkLoadState ()) .toBe (X3D .X3DConstants .NOT_STARTED_STATE);
+});
 
 test ("load-complete", async () =>
 {
@@ -167,13 +169,13 @@ EXTERNPROTO Test [
 "${url .pathToFileURL (path .join (__dirname, "files", "proto.x3d"))}"
 
 Test { }
-`)
+`);
 
-   expect (scene .rootNodes) .toHaveLength (1)
-   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Test")
-   expect (scene .externprotos [0] .loadState) .toBe (X3D .X3DConstants .COMPLETE_STATE)
-   expect (scene .externprotos [0] .checkLoadState ()) .toBe (X3D .X3DConstants .COMPLETE_STATE)
-})
+   expect (scene .rootNodes) .toHaveLength (1);
+   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Test");
+   expect (scene .externprotos [0] .loadState) .toBe (X3D .X3DConstants .COMPLETE_STATE);
+   expect (scene .externprotos [0] .checkLoadState ()) .toBe (X3D .X3DConstants .COMPLETE_STATE);
+});
 
 test ("load-failed", async () =>
 {
@@ -185,10 +187,10 @@ EXTERNPROTO Test [
 ]
 "does/not/exists.x3d"
 
-Test { }`)
+Test { }`);
 
-   expect (scene .rootNodes) .toHaveLength (1)
-   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Test")
-   expect (scene .externprotos [0] .loadState) .toBe (X3D .X3DConstants .FAILED_STATE)
-   expect (scene .externprotos [0] .checkLoadState ()) .toBe (X3D .X3DConstants .FAILED_STATE)
-})
+   expect (scene .rootNodes) .toHaveLength (1);
+   expect (scene .rootNodes [0] .getNodeTypeName ()) .toBe ("Test");
+   expect (scene .externprotos [0] .loadState) .toBe (X3D .X3DConstants .FAILED_STATE);
+   expect (scene .externprotos [0] .checkLoadState ()) .toBe (X3D .X3DConstants .FAILED_STATE);
+});
--- tests/x_ite/Prototype/X3DProtoDeclaration.test.js
+++ tests/x_ite/Prototype/X3DProtoDeclaration.test.js
@@ -1,5 +1,5 @@
 
-const X3D = require ("../../X3D")
+const X3D = require ("../../X3D");
 
 test ("properties", async () =>
 {
@@ -27,71 +27,73 @@ PROTO Test [
 }
 
 Test { }
-`))
+`));
 
-   expect (scene .protos) .toHaveLength (1)
+   expect (scene .protos) .toHaveLength (1);
 
-   const proto = scene .protos [0]
+   const proto = scene .protos [0];
 
-   expect (proto) .toBeInstanceOf (X3D .X3DProtoDeclaration)
-   expect (proto .constructor) .toBe (X3D .X3DProtoDeclaration)
-   expect (proto .name) .toBe ("Test")
-   expect (proto .isExternProto) .toBe (false)
-   expect (proto .appInfo) .toBe ("")
-   expect (proto .documentation) .toBe ("")
-   expect (proto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
-   expect (proto .fields) .toHaveLength (2)
-   expect (proto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (proto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
-   expect (proto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
-   expect (proto .fields [0] .name) .toBe ("metadata")
-   expect (proto .fields [0] .value) .toBe (null)
-   expect (proto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (proto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
-   expect (proto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
-   expect (proto .fields [1] .name) .toBe ("size")
-   expect (proto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f)
-   expect (proto .getName ()) .toBe (proto .name)
+   expect (proto) .toBeInstanceOf (X3D .X3DProtoDeclaration);
+   expect (proto .constructor) .toBe (X3D .X3DProtoDeclaration);
+   expect (proto .name) .toBe ("Test");
+   expect (proto .isExternProto) .toBe (false);
+   expect (proto .appInfo) .toBe ("");
+   expect (proto .documentation) .toBe ("");
+   expect (proto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray);
+   expect (proto .fields) .toHaveLength (2);
+   expect (proto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (proto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput);
+   expect (proto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode);
+   expect (proto .fields [0] .name) .toBe ("metadata");
+   expect (proto .fields [0] .value) .toBe (null);
+   expect (proto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (proto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly);
+   expect (proto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f);
+   expect (proto .fields [1] .name) .toBe ("size");
+   expect (proto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f);
+   expect (proto .getName ()) .toBe (proto .name);
 
-   expect (X3D .X3DProtoDeclaration .typeName) .toBe ("X3DProtoDeclaration")
-   expect (proto .getTypeName ()) .toBe ("X3DProtoDeclaration")
-   expect (Object .prototype .toString .call (proto)) .toBe (`[object X3DProtoDeclaration]`)
-   expect (proto .toString ()) .toBe (`[object ${proto .getTypeName ()}]`)
+   expect (X3D .X3DProtoDeclaration .typeName) .toBe ("X3DProtoDeclaration");
+   expect (proto .getTypeName ()) .toBe ("X3DProtoDeclaration");
+   expect (Object .prototype .toString .call (proto)) .toBe (`[object X3DProtoDeclaration]`);
+   expect (proto .toString ()) .toBe (`[object ${proto .getTypeName ()}]`);
 
-   proto .name = undefined
-   proto .isExternProto = undefined
-   proto .appInfo = undefined
-   proto .documentation = undefined
-   proto .fields = undefined
+   proto .name = undefined;
+   proto .isExternProto = undefined;
+   proto .appInfo = undefined;
+   proto .documentation = undefined;
+   proto .fields = undefined;
 
-   expect (proto .name) .toBe ("Test")
-   expect (proto .isExternProto) .toBe (false)
-   expect (proto .appInfo) .toBe ("")
-   expect (proto .documentation) .toBe ("")
-   expect (proto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray)
-   expect (proto .fields) .toHaveLength (2)
-   expect (proto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (proto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput)
-   expect (proto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode)
-   expect (proto .fields [0] .name) .toBe ("metadata")
-   expect (proto .fields [0] .value) .toBe (null)
-   expect (proto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition)
-   expect (proto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly)
-   expect (proto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f)
-   expect (proto .fields [1] .name) .toBe ("size")
-   expect (proto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f)
+   expect (proto .name) .toBe ("Test");
+   expect (proto .isExternProto) .toBe (false);
+   expect (proto .appInfo) .toBe ("");
+   expect (proto .documentation) .toBe ("");
+   expect (proto .fields) .toBeInstanceOf (X3D .FieldDefinitionArray);
+   expect (proto .fields) .toHaveLength (2);
+   expect (proto .fields [0]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (proto .fields [0] .accessType) .toBe (X3D .X3DConstants .inputOutput);
+   expect (proto .fields [0] .dataType) .toBe (X3D .X3DConstants .SFNode);
+   expect (proto .fields [0] .name) .toBe ("metadata");
+   expect (proto .fields [0] .value) .toBe (null);
+   expect (proto .fields [1]) .toBeInstanceOf (X3D .X3DFieldDefinition);
+   expect (proto .fields [1] .accessType) .toBe (X3D .X3DConstants .initializeOnly);
+   expect (proto .fields [1] .dataType) .toBe (X3D .X3DConstants .SFVec3f);
+   expect (proto .fields [1] .name) .toBe ("size");
+   expect (proto .fields [1] .value) .toBeInstanceOf (X3D .SFVec3f);
 
-   const instance = proto .newInstance ()
+   const instance = proto .newInstance ();
 
-   expect (instance) .toBeInstanceOf (X3D .SFNode)
-   expect (instance .getNodeTypeName ()) .toBe ("Test")
+   expect (instance) .toBeInstanceOf (X3D .SFNode);
+   expect (instance .getValue ()) .toBeInstanceOf (X3D .X3DPrototypeInstance);
+   expect (instance .getValue () .isInitialized ()) .toBe (true);
+   expect (instance .getNodeTypeName ()) .toBe ("Test");
 
-   const body = instance .getValue () .getBody ()
+   const body = instance .getValue () .getBody ();
 
-   expect (body .rootNodes) .toHaveLength (2)
-   expect (body .protos) .toHaveLength (0)
-   expect (body .externprotos) .toHaveLength (0)
-   expect (body .routes) .toHaveLength (1)
+   expect (body .rootNodes) .toHaveLength (2);
+   expect (body .protos) .toHaveLength (0);
+   expect (body .externprotos) .toHaveLength (0);
+   expect (body .routes) .toHaveLength (1);
 
    const properties = [
       "name",
@@ -101,5 +103,5 @@ Test { }
       "documentation",
    ]
 
-   enumerate (properties, proto)
-})
+   enumerate (properties, proto);
+});
