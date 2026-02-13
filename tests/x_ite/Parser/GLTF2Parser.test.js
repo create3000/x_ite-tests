3cc5095b57acaba82419433b84ac081b01040779
Holger Seelig <holger.seelig@gmail.com>
2026-02-13 12:37:13 +0100

    Updated tests.

--- tests/x_ite/Parser/GLTF2Parser.test.js
+++ tests/x_ite/Parser/GLTF2Parser.test.js
@@ -1,49 +1,49 @@
 const
    path = require ("path"),
-   url  = require ("url")
+   url  = require ("url");
 
-const X3D = require ("../../X3D")
+const X3D = require ("../../X3D");
 
 test ("glTF", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF", "CesiumMan.gltf"))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF", "CesiumMan.gltf"))));
 
-   expect (scene .encoding) .toBe ("GLTF")
-   expect (scene .rootNodes .length) .toBe (3)
-})
+   expect (scene .encoding) .toBe ("GLTF");
+   expect (scene .rootNodes .length) .toBe (2);
+});
 
 test ("glTF-Binary", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF-Binary", "CesiumMan.glb"))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF-Binary", "CesiumMan.glb"))));
 
-   expect (scene .encoding) .toBe ("GLTF")
-   expect (scene .rootNodes .length) .toBe (3)
-})
+   expect (scene .encoding) .toBe ("GLTF");
+   expect (scene .rootNodes .length) .toBe (2);
+});
 
 test ("glTF-Draco", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF-Draco", "CesiumMan.gltf"))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF-Draco", "CesiumMan.gltf"))));
 
-   expect (scene .encoding) .toBe ("GLTF")
-   expect (scene .rootNodes .length) .toBe (3)
-})
+   expect (scene .encoding) .toBe ("GLTF");
+   expect (scene .rootNodes .length) .toBe (2);
+});
 
 test ("glTF-Embedded", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF-Embedded", "CesiumMan.gltf"))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "glTF", "CesiumMan", "glTF-Embedded", "CesiumMan.gltf"))));
 
-   expect (scene .encoding) .toBe ("GLTF")
-   expect (scene .rootNodes .length) .toBe (3)
-})
+   expect (scene .encoding) .toBe ("GLTF");
+   expect (scene .rootNodes .length) .toBe (2);
+});
--- tests/x_ite/Parser/STLBParser.test.js
+++ tests/x_ite/Parser/STLBParser.test.js
@@ -1,38 +1,38 @@
 const
    path = require ("path"),
-   url  = require ("url")
+   url  = require ("url");
 
-const X3D = require ("../../X3D")
+const X3D = require ("../../X3D");
 
 test ("cube.stl", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "STL", `cube.stl`))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "STL", `cube.stl`))));
 
-   expect (scene .encoding) .toBe ("STL")
-   expect (scene .rootNodes) .toHaveLength (1)
+   expect (scene .encoding) .toBe ("STL");
+   expect (scene .rootNodes) .toHaveLength (2);
 
-   const shape = scene .rootNodes [0]
+   const shape = scene .rootNodes [1];
 
-   expect (shape .getNodeTypeName ()) .toBe ("Shape")
-   expect (shape .appearance .getNodeTypeName ()) .toBe ("Appearance")
-   expect (shape .appearance .material .getNodeTypeName ()) .toBe ("Material")
-   expect (shape .geometry .getNodeTypeName ()) .toBe ("TriangleSet")
-   expect (shape .geometry .normal .getNodeTypeName ()) .toBe ("Normal")
-   expect (shape .geometry .normal .vector) .toHaveLength (12)
-   expect (shape .geometry .coord .getNodeTypeName ()) .toBe ("Coordinate")
-   expect (shape .geometry .coord .point) .toHaveLength (36)
-})
+   expect (shape .getNodeTypeName ()) .toBe ("Shape");
+   expect (shape .appearance .getNodeTypeName ()) .toBe ("Appearance");
+   expect (shape .appearance .material .getNodeTypeName ()) .toBe ("Material");
+   expect (shape .geometry .getNodeTypeName ()) .toBe ("TriangleSet");
+   expect (shape .geometry .normal .getNodeTypeName ()) .toBe ("Normal");
+   expect (shape .geometry .normal .vector) .toHaveLength (12);
+   expect (shape .geometry .coord .getNodeTypeName ()) .toBe ("Coordinate");
+   expect (shape .geometry .coord .point) .toHaveLength (36);
+});
 
 test ("Menger_sponge-Binary.stl", async () =>
 {
    const
       canvas  = X3D .createBrowser (),
       Browser = canvas .browser,
-      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "STL", `Menger_sponge-Binary.stl`))))
+      scene   = await Browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "files", "STL", `Menger_sponge-Binary.stl`))));
 
-   expect (scene .encoding) .toBe ("STL")
-   expect (scene .rootNodes) .toHaveLength (1)
-})
+   expect (scene .encoding) .toBe ("STL");
+   expect (scene .rootNodes) .toHaveLength (2);
+});
