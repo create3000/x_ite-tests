198ff22404b21ec03dd178ac44a0b7724599e8de
Holger Seelig <holger.seelig@gmail.com>
2026-05-21 12:26:54 +0200

    Removed useless x.

--- tests/x_ite/Fields/MFNode.test.js
+++ tests/x_ite/Fields/MFNode.test.js
@@ -307,17 +307,16 @@ test ("entries", () =>
 {
    const
       N = 10,
-      a = new MFNode (),
-      x = [ ]
+      a = new MFNode ();
 
    for (let i = 0; i < N; ++ i)
-      expect (a .push (x [i] = scene .createNode ("WorldInfo"))) .toBe (i + 1)
+      expect (a .push (scene .createNode ("WorldInfo"))) .toBe (i + 1);
 
-   expect ([... a .entries ()]) .toHaveLength (N)
+   expect ([... a .entries ()]) .toHaveLength (N);
 
    for (const [i, value] of a .entries ())
-      expect (value) .toBe (a [i])
-})
+      expect (value) .toBe (a [i]);
+});
 
 test ("fill", () =>
 {
