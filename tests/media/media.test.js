c93c2f6144712a1ed3c7566f2fdebcdfd87d3342
Holger Seelig <holger.seelig@gmail.com>
2026-07-12 10:50:51 +0200

    Reorder functions.

--- tests/media/media.test.js
+++ tests/media/media.test.js
@@ -27,79 +27,6 @@ browser .setBrowserOption ("Mute", true);
 
 test ("media", async () =>
 {
-   function loadImage (url)
-   {
-      return new Promise ((resolve, reject) =>
-      {
-         const image = new Image ();
-
-         image .onload = () => resolve (image);
-
-         image .onerror =
-         image .onabort = event => reject (new Error (`Couldn't load image '${url}': ${event .type}.`));
-
-         image .src = url;
-      });
-   }
-
-   function readPixels (image)
-   {
-      const
-         gl          = browser .getContext (),
-         texture     = gl .createTexture (),
-         framebuffer = gl .createFramebuffer (),
-         width       = image .width,
-         height      = image .height,
-         data        = new Uint8Array (width * height * 4);
-
-      // Create texture.
-
-      gl .bindTexture (gl .TEXTURE_2D, texture);
-      gl .pixelStorei (gl .UNPACK_COLORSPACE_CONVERSION_WEBGL, gl .NONE);
-      gl .texImage2D  (gl .TEXTURE_2D, 0, gl .RGBA, width, height, 0, gl .RGBA, gl .UNSIGNED_BYTE, image);
-      gl .pixelStorei (gl .UNPACK_COLORSPACE_CONVERSION_WEBGL, gl .BROWSER_DEFAULT_WEBGL);
-
-      // Read pixels from framebuffer.
-
-      gl .bindFramebuffer (gl .FRAMEBUFFER, framebuffer);
-      gl .framebufferTexture2D (gl .FRAMEBUFFER, gl .COLOR_ATTACHMENT0, gl .TEXTURE_2D, texture, 0);
-      gl .readPixels (0, 0, width, height, gl .RGBA, gl .UNSIGNED_BYTE, data);
-
-      // Clean up.
-
-      gl .deleteFramebuffer (framebuffer);
-      gl .deleteTexture (texture);
-
-      return data;
-   }
-
-   async function run (command, args = [ ], stdinBlob = null)
-   {
-      return new Promise ((resolve, reject) =>
-      {
-         const child = child_process .spawn (command, args);
-
-         const stdoutChunks = [];
-         const stderrChunks = [];
-
-         child.stdout.on("data", chunk => stdoutChunks .push (chunk));
-
-         child .stderr .on( "data", chunk => stderrChunks .push (chunk));
-
-         child .on ("error", reject);
-
-         child .on ("close", () => resolve (new Blob (stdoutChunks)));
-
-         (async () =>
-         {
-            const buffer = Buffer .from (await stdinBlob .arrayBuffer ());
-
-            child.stdin .end (buffer);
-         })()
-         .catch (reject);
-      });
-   }
-
    const media = require ("../../../media/docs/examples/config.json");
 
    for (const example of media)
@@ -156,3 +83,76 @@ test ("media", async () =>
    }
 },
 70_000);
+
+function loadImage (url)
+{
+   return new Promise ((resolve, reject) =>
+   {
+      const image = new Image ();
+
+      image .onload = () => resolve (image);
+
+      image .onerror =
+      image .onabort = event => reject (new Error (`Couldn't load image '${url}': ${event .type}.`));
+
+      image .src = url;
+   });
+}
+
+function readPixels (image)
+{
+   const
+      gl          = browser .getContext (),
+      texture     = gl .createTexture (),
+      framebuffer = gl .createFramebuffer (),
+      width       = image .width,
+      height      = image .height,
+      data        = new Uint8Array (width * height * 4);
+
+   // Create texture.
+
+   gl .bindTexture (gl .TEXTURE_2D, texture);
+   gl .pixelStorei (gl .UNPACK_COLORSPACE_CONVERSION_WEBGL, gl .NONE);
+   gl .texImage2D  (gl .TEXTURE_2D, 0, gl .RGBA, width, height, 0, gl .RGBA, gl .UNSIGNED_BYTE, image);
+   gl .pixelStorei (gl .UNPACK_COLORSPACE_CONVERSION_WEBGL, gl .BROWSER_DEFAULT_WEBGL);
+
+   // Read pixels from framebuffer.
+
+   gl .bindFramebuffer (gl .FRAMEBUFFER, framebuffer);
+   gl .framebufferTexture2D (gl .FRAMEBUFFER, gl .COLOR_ATTACHMENT0, gl .TEXTURE_2D, texture, 0);
+   gl .readPixels (0, 0, width, height, gl .RGBA, gl .UNSIGNED_BYTE, data);
+
+   // Clean up.
+
+   gl .deleteFramebuffer (framebuffer);
+   gl .deleteTexture (texture);
+
+   return data;
+}
+
+async function run (command, args = [ ], stdinBlob = null)
+{
+   return new Promise ((resolve, reject) =>
+   {
+      const child = child_process .spawn (command, args);
+
+      const stdoutChunks = [];
+      const stderrChunks = [];
+
+      child.stdout.on("data", chunk => stdoutChunks .push (chunk));
+
+      child .stderr .on( "data", chunk => stderrChunks .push (chunk));
+
+      child .on ("error", reject);
+
+      child .on ("close", () => resolve (new Blob (stdoutChunks)));
+
+      (async () =>
+      {
+         const buffer = Buffer .from (await stdinBlob .arrayBuffer ());
+
+         child.stdin .end (buffer);
+      })()
+      .catch (reject);
+   });
+}
