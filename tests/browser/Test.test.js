77ac7d5b0a0cd8c79b1e6e64bebb97b78918ea93
Holger Seelig <holger.seelig@gmail.com>
2026-07-15 23:13:45 +0200

    Added tests.

--- tests/browser/Test.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import X3D              from "https://weiputer/x_ite/dist/x_ite.mjs";
-
-test ("test", () =>
-{
-   expect (window) .toBeInstanceOf (Object);
-   expect (X3D) .toBeInstanceOf (Object);
-});
