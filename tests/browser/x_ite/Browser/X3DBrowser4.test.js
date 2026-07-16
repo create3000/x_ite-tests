d3f7f19decbdc2b8df2193640f29d3c763e51cf0
Holger Seelig <holger.seelig@gmail.com>
2026-07-16 11:04:33 +0200

    Updated test.

--- tests/browser/x_ite/Browser/X3DBrowser4.test.js
+++ tests/browser/x_ite/Browser/X3DBrowser4.test.js
@@ -1,12 +1,12 @@
 import { expect, test } from "vitest";
 import X3D              from "../../X3D.js";
 
-const
-   canvas = X3D .createBrowser (),
-   Browser = canvas .browser;
-
 test .concurrent ("KelpForestMain.x3d", async () =>
 {
+   const
+      canvas = X3D .createBrowser (),
+      Browser = canvas .browser;
+
    const kelp = "https://www.web3d.org/x3d/content/examples/X3dForWebAuthors/KelpForestExhibit/KelpForestMain.x3d";
 
    await Browser .loadURL (new X3D .MFString (kelp));
