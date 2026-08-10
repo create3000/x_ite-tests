f04e88e5d41fb7d96088f54cf882770912f8739f
Holger Seelig <holger.seelig@gmail.com>
2026-08-10 16:05:21 +0200

    Reverted changes.

--- tests/browser/X3D.js
+++ tests/browser/X3D.js
@@ -1,21 +1,5 @@
 import { expect } from "vitest";
-import X3D              from "https://weiputer/x_ite/dist/x_ite.mjs";
-
-function getPropertyDescriptor (obj, key)
-{
-	if (obj === undefined || obj === null)
-		throw new TypeError('Cannot convert undefined or null to object');
-
-	if (key in obj)
-		return getRecursivePropertyDescriptor (obj);
-
-	function getRecursivePropertyDescriptor (obj)
-   {
-		return Object .prototype .hasOwnProperty .call (obj, key)
-			? Object .getOwnPropertyDescriptor (obj, key)
-		   : getRecursivePropertyDescriptor (Object .getPrototypeOf (obj));
-	}
-}
+import X3D        from "https://weiputer/x_ite/dist/x_ite.mjs";
 
 Object .assign (window,
 {
@@ -26,10 +10,7 @@ Object .assign (window,
          b = { };
 
       for (const property in target)
-      {
-         if (getPropertyDescriptor (target, property) ?.enumerable)
-            a [property] = true;
-      }
+         a [property] = true;
 
       for (const property of properties)
          b [property] = true;
