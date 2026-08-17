81f5ff581ce3576034ee71374e0b4b724c861683
Holger Seelig <holger.seelig@gmail.com>
2026-08-17 18:31:09 +0200

    Test constructor.

--- tests/browser/x_ite/Fields/MFImage.test.js
+++ tests/browser/x_ite/Fields/MFImage.test.js
@@ -13,6 +13,45 @@ test .concurrent ("constructor static methods", () =>
    expect (MFImage .fromArray (a .flat ()) .equals (a)) .toBe (true);
 });
 
+test .concurrent ("constructor", () =>
+{
+   const a = new MFImage ();
+
+   expect (a) .toHaveLength (0);
+   expect (a [0]) .toBe (undefined);
+   expect (a) .toHaveLength (0);
+
+   const b = new MFImage (new SFImage (1,2,3,new MFInt32(0xa,0xb)), new SFImage (2,1,4,new MFInt32(0xc,0xd)));
+   expect (b) .toHaveLength (2);
+   expect (b [0] .equals (new SFImage (1,2,3,new MFInt32(0xa,0xb)))) .toBe (true);
+   expect (b [1] .equals (new SFImage (2,1,4,new MFInt32(0xc,0xd)))) .toBe (true);
+
+   const c = [... b];
+   expect (c) .toHaveLength (2);
+   expect (c [0] .equals (new SFImage (1,2,3,new MFInt32(0xa,0xb)))) .toBe (true);
+   expect (c [1] .equals (new SFImage (2,1,4,new MFInt32(0xc,0xd)))) .toBe (true);
+   for (let i = 0; i < 2; ++ i)
+      expect (c [i]) .toBe (b [i]);
+
+   const d = new MFImage (new SFImage (1,2,3,new MFInt32(0xa,0xb)));
+   expect (d) .toHaveLength (1);
+   expect (d [0] .equals (new SFImage (1,2,3,new MFInt32(0xa,0xb)))) .toBe (true);
+   d [0] .width  = 2;
+   d [0] .height = 2;
+   d [0] .comp   = 3;
+   d [0] .array  = new MFInt32 (1,2,3,4);
+   d .length = 2;
+   d [1] .width  = 3;
+   d [1] .height = 3;
+   d [1] .comp   = 3;
+   d [1] .array  = new MFInt32 (1,2,3,4,5,6,7,8,9);
+   expect (d) .toHaveLength (2);
+   expect (d [0] .equals (new SFImage (2,2,3,new MFInt32(1,2,3,4)))) .toBe (true);
+   expect (d [1] .equals (new SFImage (3,3,3,new MFInt32(1,2,3,4,5,6,7,8,9)))) .toBe (true);
+
+   expect ((new MFImage ()) [0]) .toBe (undefined);
+});
+
 test .concurrent ("equals", () =>
 {
    const
