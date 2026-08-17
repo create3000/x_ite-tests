399d380b2f55bc1deadf9a4403bc3d4b59f87aea
Holger Seelig <holger.seelig@gmail.com>
2026-08-17 18:11:13 +0200

    Test flat.

--- tests/browser/x_ite/Fields/MFImage.test.js
+++ tests/browser/x_ite/Fields/MFImage.test.js
@@ -12,3 +12,15 @@ test .concurrent ("constructor static methods", () =>
    expect (MFImage .from (a) .equals (a)) .toBe (true);
    expect (MFImage .fromArray (a .flat ()) .equals (a)) .toBe (true);
 });
+
+test .concurrent ("flat", () =>
+{
+   const
+      a = new MFImage (),
+      b = new MFImage (new SFImage (1,1,3,new MFInt32(0xaaa)), new SFImage (1,1,3,new MFInt32(0xbbb)));
+
+   expect (a .flat ()) .toBeInstanceOf (Array);
+   expect (a .flat ()) .toEqual ([ ]);
+   expect (b .flat ()) .toBeInstanceOf (Array);
+   expect (b .flat ()) .toEqual ([1,1,3,0xaaa,1,1,3,0xbbb]);
+});
