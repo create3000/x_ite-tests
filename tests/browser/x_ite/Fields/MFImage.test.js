e9e1538b16fcd665eeec0b30cea2fefacb6a0a1f
Holger Seelig <holger.seelig@gmail.com>
2026-08-17 17:13:45 +0200

    Added test file for MFImage.

---
+++ tests/browser/x_ite/Fields/MFImage.test.js
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+import X3D              from "../../X3D.js";
+
+const MFImage = X3D .MFImage;
+const MFInt32 = X3D .MFInt32;
+const SFImage = X3D .SFImage;
+
+test .concurrent ("constructor static methods", () =>
+{
+   const a = new MFImage (new SFImage (1,1,3,new MFInt32(0xaaa)), new SFImage (1,1,3,new MFInt32(0xbbb)));
+
+   expect (MFImage .from (a) .equals (a)) .toBe (true);
+   expect (MFImage .fromArray (a .flat ()) .equals (a)) .toBe (true);
+});
