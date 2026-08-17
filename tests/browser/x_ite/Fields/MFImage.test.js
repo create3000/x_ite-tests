f4c34b40d95b04af84b2a2ce28d4656797459779
Holger Seelig <holger.seelig@gmail.com>
2026-08-17 18:14:25 +0200

    Test equals.

--- tests/browser/x_ite/Fields/MFImage.test.js
+++ tests/browser/x_ite/Fields/MFImage.test.js
@@ -7,20 +7,33 @@ const SFImage = X3D .SFImage;
 
 test .concurrent ("constructor static methods", () =>
 {
-   const a = new MFImage (new SFImage (1,1,3,new MFInt32(0xaaa)), new SFImage (1,1,3,new MFInt32(0xbbb)));
+   const a = new MFImage (new SFImage (1,2,3,new MFInt32(0xa,0xb)), new SFImage (2,1,4,new MFInt32(0xc,0xd)));
 
    expect (MFImage .from (a) .equals (a)) .toBe (true);
    expect (MFImage .fromArray (a .flat ()) .equals (a)) .toBe (true);
+});
+
+test .concurrent ("equals", () =>
+{
+   const
+      a = new MFImage (),
+      b = new MFImage (new SFImage (1,2,3,new MFInt32(0xa,0xb)), new SFImage (2,1,4,new MFInt32(0xc,0xd))),
+      c = new MFImage (new SFImage (1,2,3,new MFInt32(0xa,0xb)), new SFImage (2,1,4,new MFInt32(0xc,0xd)));
+
+   expect (a .equals (a)) .toBe (true);
+   expect (b .equals (b)) .toBe (true);
+   expect (a .equals (b)) .toBe (false);
+   expect (b .equals (c)) .toBe (true);
 });
 
 test .concurrent ("flat", () =>
 {
    const
       a = new MFImage (),
-      b = new MFImage (new SFImage (1,1,3,new MFInt32(0xaaa)), new SFImage (1,1,3,new MFInt32(0xbbb)));
+      b = new MFImage (new SFImage (1,2,3,new MFInt32(0xa,0xb)), new SFImage (2,1,4,new MFInt32(0xc,0xd)));
 
    expect (a .flat ()) .toBeInstanceOf (Array);
    expect (a .flat ()) .toEqual ([ ]);
    expect (b .flat ()) .toBeInstanceOf (Array);
-   expect (b .flat ()) .toEqual ([1,1,3,0xaaa,1,1,3,0xbbb]);
+   expect (b .flat ()) .toEqual ([1,2,3,0xa,0xb,2,1,4,0xc,0xd]);
 });
