351add4db4ce1582f561799d158207de10932206
Holger Seelig <holger.seelig@gmail.com>
2026-09-13 01:44:25 +0200

    Updated formatting.

--- tests/browser/x_ite/Execution/X3DExecutionContext.test.js
+++ tests/browser/x_ite/Execution/X3DExecutionContext.test.js
@@ -945,11 +945,9 @@ DEF T4 Transform { }
    expect (scene .routes) .toHaveLength (0);
 
    const tRoute = scene .addRoute (t1, "translation", t2, "translation");
-
    expect (scene .routes) .toHaveLength (1);
 
    scene .deleteRoute (tRoute);
-
    expect (scene .routes) .toHaveLength (0);
 
    expect (() => scene .addRoute (t1, "translation", t2, "rotation")) .toThrow (Error);
